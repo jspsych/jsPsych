5ffc88513978970ee7876380a003993901b3d885
Josh de Leeuw <josh.deleeuw@gmail.com>
2012-04-23 20:56:18 -0400

    added a touch version of the xab task (click on the target instead of pressing a key)

--- jspsych-xab.js
+++ jspsych-xab.js
@@ -10,6 +10,9 @@ function xab_create(params)
 		trials[i]["a_path"] = xab_stims[i][0];
 		trials[i]["b_path"] = xab_stims[i][1];
 		trials[i]["timing"] = params["timing"];
+		if(params["data"]!=undefined){
+			trials[i]["data"] = params["data"][i];
+		}
 	}
 	return trials;
 }
@@ -18,16 +21,19 @@ function xab_trial($this, block, trial, part)
 {
 	switch(part){
 		case 1:
+			p1_time = (new Date()).getTime();
 			$.fn.jsPsych.showImage($this, trial.a_path, 'xab');
 			setTimeout(xab_trial, trial.timing[0], $this, block, trial, part + 1);
 			break;
 		case 2:
+			p2_time = (new Date()).getTime();
 			$('.xab').remove();
 			setTimeout(xab_trial, trial.timing[1], $this, block, trial, part + 1);
 			break;
 		case 3:
+			p3_time = (new Date()).getTime();
 			startTime = (new Date()).getTime();
-			var images = [trial.a_path,trial.b_path];
+			var images = [trial.a_path, trial.b_path];
 			var target_left = (Math.floor(Math.random()*2)==0); // binary true/false choice
 			if(!target_left){
 				images = [trial.b_path, trial.a_path];
@@ -49,7 +55,10 @@ function xab_trial($this, block, trial, part)
 				{
 					endTime = (new Date()).getTime();
 					rt = (endTime-startTime);
-					block.data[block.trial_idx] = {"rt": rt, "correct": correct, "a_path": trial.a_path, "b_path": trial.b_path, "keypress": e.which}
+					stim1_time = (p2_time-p1_time);
+					isi_time = (p3_time-p2_time);
+					var trial_data = {"rt": rt, "correct": correct, "a_path": trial.a_path, "b_path": trial.b_path, "key_press": e.which, "key_press": e.which, "stim1_time": stim1_time, "isi_time":isi_time}
+					block.data[block.trial_idx] = $.extend({},trial_data,trial.data);
 					$(document).unbind('keyup',resp_func);
 					$('.xab').remove();
 					setTimeout(function(b){b.next();}, trial.timing[2], block);
