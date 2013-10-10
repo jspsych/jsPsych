b0986a1a1952f742cdc77df4f732cb7151c0ee32
Josh de Leeuw <josh.deleeuw@gmail.com>
2013-10-10 12:05:21 -0400

    bug fix utils

--- utils/jspsych-utils.js
+++ utils/jspsych-utils.js
@@ -11,9 +11,12 @@
         // a one-dimensional array where each entry is data from a  single trial
         // append_data is optional and can be items that should be added to all
         // trials, such as a subject identifier
-        public_object.flatten_data_object = function(data_object, append_data = {}) {
+        public_object.flatten_data_object = function(data_object, append_data) {
+            
+            append_data = (typeof append_data===undefined) ? {} : append_data;
+            
             var trials = [];
-
+            
             // loop through data_object
             for (var i = 0; i < data_object.length; i++) {
                 for (var j = 0; j < data_object[i].length; j++) {
@@ -124,7 +127,7 @@
             $('#stim_palmer').remove();
 
             return svg;
-        }
+        };
 
         return public_object;
     })();
