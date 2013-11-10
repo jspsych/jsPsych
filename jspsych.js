1f173bcea42f54accb4f5391477215b5ac830c11
Josh de Leeuw <josh.deleeuw@gmail.com>
2013-11-09 19:59:27 -0500

    tweaking saveAstext

--- jspsych.js
+++ jspsych.js
@@ -254,7 +254,6 @@
         // http://stackoverflow.com/users/317/joseph-sturtevant
         function JSON2CSV(objArray) {
             var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
-
             var line = '';
             var result = '';
             var columns = [];
@@ -264,7 +263,7 @@
                 for (var key in array[j]) {
                     var keyString = key + "";
                     keyString = '"' + keyString.replace(/"/g, '""') + '",';
-                    if (columns.indexOf(key) == -1) {
+                    if ($.inArray(key,columns) == -1) {
                         columns[i] = key;
                         line += keyString;
                         i++;
@@ -272,19 +271,16 @@
                 }
             }
 
-
             line = line.slice(0, - 1);
             result += line + '\r\n';
 
             for (var i = 0; i < array.length; i++) {
                 var line = '';
-
                 for (var j = 0; j < columns.length; j++) {
                     var value = (typeof array[i][columns[j]] === 'undefined') ? '' : array[i][columns[j]];
                     var valueString = value + "";
                     line += '"' + valueString.replace(/"/g, '""') + '",';
                 }
-
 
                 line = line.slice(0, - 1);
                 result += line + '\r\n';
