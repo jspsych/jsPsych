6aed75b9d1b3ddea00a3c544f19166d39fbd698a
Josh de Leeuw <josh.deleeuw@gmail.com>
2013-12-10 11:21:20 -0500

    revising sim plugin to provide more flexibility in how the trial unfolds - related to #12

--- plugins/jspsych-similarity.js
+++ plugins/jspsych-similarity.js
@@ -31,8 +31,12 @@
                 trials[i].a_path = params.stimuli[i][0];
                 trials[i].b_path = params.stimuli[i][1];
 
-                trials[i].label_low = params.label_low || "Not at all similar";
-                trials[i].label_high = params.label_high || "Identical";
+                // TODO make all changes related to following 4 parameters.
+                trials[i].labels = (typeof params.labels === 'undefined') ? ["Not at all similar", "Identical"] : params.labels;
+                trials[i].intervals = params.intervals || 100;
+                trials[i].show_ticks = (typeof params.show_ticks === 'undefined') ? false : params.show_ticks;
+
+                trials[i].show_response = params.show_response || "SECOND_STIMULUS";
 
                 trials[i].timing_first_stim = params.timing_first_stim || 1000; // default 1000ms
                 trials[i].timing_second_stim = params.timing_second_stim || -1; // -1 = inf time; positive numbers = msec to display second image.
@@ -56,14 +60,18 @@
                 if (!trial.is_html) {
                     display_element.append($('<img>', {
                         "src": trial.a_path,
-                        "class": 'sim'
+                        "id": 'jspsych_sim_stim'
                     }));
                 }
                 else {
                     display_element.append($('<div>', {
                         "html": trial.a_path,
-                        "class": 'sim'
+                        "id": 'jspsych_sim_stim'
                     }));
+                }
+
+                if (trial.show_response == "FIRST_STIMULUS") {
+                    show_response_slider(display_element, trial, block);
                 }
 
                 setTimeout(function() {
@@ -73,7 +81,7 @@
 
             case 2:
 
-                $('.sim').remove();
+                $('#jspsych_sim_stim').css('visibility', 'hidden');
 
                 setTimeout(function() {
                     plugin.trial(display_element, block, trial, part + 1);
@@ -82,103 +90,133 @@
             case 3:
 
                 if (!trial.is_html) {
-                    display_element.append($('<img>', {
-                        "src": trial.b_path,
-                        "class": 'sim',
-                        "id": 'jspsych_sim_second_image'
-                    }));
+                    $('#jspsych_sim_stim').attr('src', trial.b_path);
                 }
                 else {
-                    display_element.append($('<div>', {
-                        "html": trial.b_path,
-                        "class": 'sim',
-                        "id": 'jspsych_sim_second_image'
-                    }));
+                    $('#jspsych_sim_stim').html(trial.b_path);
+                }
+
+                if (trial.show_response == "SECOND_STIMULUS") {
+                    show_response_slider(display_element, trial, block);
                 }
 
                 if (trial.timing_second_stim > 0) {
                     setTimeout(function() {
                         if (!sim_trial_complete) {
-                            $("#jspsych_sim_second_image").css('visibility', 'hidden');
+                            $("#jspsych_sim_stim").css('visibility', 'hidden');
+                            if (trial.show_response == "POST_STIMULUS") {
+                                show_response_slider(display_element, trial, block);
+                            }
                         }
                     }, trial.timing_second_stim);
                 }
 
-                // create slider
-                display_element.append($('<div>', {
-                    "id": 'slider',
-                    "class": 'sim'
-                }));
-                $("#slider").slider({
-                    value: 50,
-                    min: 0,
-                    max: 100,
-                    step: 1,
-                });
-
-
-                // create labels for slider
-                display_element.append($('<div>', {
-                    "id": 'slider_labels',
-                    "class": 'sim',
-                    css: {
-                        height: '3em'
-                    }
-                }));
-
-                $('#slider_labels').append($('<p>',{
-                    'class': 'slider_left sim',
-                    html: trial.label_low,
-                    css: {
-                        float: 'left'
-                    }
-                }));
-                $('#slider_labels').append($('<p>',{
-                    'class': 'slider_right sim',
-                    html: trial.label_high,
-                    css: {
-                        float: 'right'
-                    }
-                }));
-                
-                //  create button
-                display_element.append($('<button>', {
-                    'id': 'next',
-                    'class': 'sim',
-                    'html': 'Submit Answer'
-                }));
-                
-                // if prompt is set, show prompt
-                if (trial.prompt !== "") {
-                    display_element.append(trial.prompt);
-                }
-
-                $("#next").click(function() {
-                    var endTime = (new Date()).getTime();
-                    var response_time = endTime - startTime;
-                    sim_trial_complete = true;
-                    var score = $("#slider").slider("value");
-                    block.writeData($.extend({}, {
-                        "sim_score": score,
-                        "rt": response_time,
-                        "stimulus": trial.a_path,
-                        "stimulus_2": trial.b_path,
-                        "trial_type": "similarity",
-                        "trial_index": block.trial_idx
-                    }, trial.data));
-                    // goto next trial in block
-                    display_element.html('');
-                    setTimeout(function() {
-                        block.next();
-                    }, trial.timing_post_trial);
-                });
-
-                var startTime = (new Date()).getTime();
                 break;
-
-
             }
         };
+
+        function show_response_slider(display_element, trial, block) {
+
+            var startTime = (new Date()).getTime();
+
+            // create slider
+            display_element.append($('<div>', {
+                "id": 'slider',
+                "class": 'sim'
+            }));
+
+            $("#slider").slider({
+                value: Math.ceil(trial.intervals / 2),
+                min: 1,
+                max: trial.intervals,
+                step: 1,
+            });
+
+            // show tick marks
+            if (trial.show_ticks) {
+                for (var j = 1; j < trial.intervals - 1; j++) {
+                    $('#slider').append('<div class="slidertickmark"></div>');
+                }
+
+                $('#slider .slidertickmark').each(function(index) {
+                    var left = (index + 1) * (100 / (trial.intervals - 1));
+                    $(this).css({
+                        'position': 'absolute',
+                        'left': left + '%',
+                        'width': '1px',
+                        'height': '100%',
+                        'background-color': '#222222'
+                    });
+                });
+            }
+
+            // create labels for slider
+            display_element.append($('<ul>', {
+                "id": "sliderlabels",
+                "class": 'sliderlabels',
+                "css": {
+                    "width": "100%",
+                    "margin": "10px 0px 0px 0px",
+                    "padding": "0px",
+                    "display": "block",
+                    "position": "relative"
+                }
+            }));
+
+            for (var j = 0; j < trial.labels.length; j++) {
+                $("#sliderlabels").append('<li>' + trial.labels[j] + '</li>');
+            }
+
+            // position labels to match slider intervals
+            var slider_width = $("#slider").width();
+            var num_items = trial.labels.length;
+            var item_width = slider_width / num_items;
+            var spacing_interval = slider_width / (num_items - 1);
+
+            $("#sliderlabels li").each(function(index) {
+                $(this).css({
+                    'display': 'inline-block',
+                    'width': item_width + 'px',
+                    'margin': '0px',
+                    'padding': '0px',
+                    'text-align': 'center',
+                    'position': 'absolute',
+                    'left': (spacing_interval * index) - (item_width / 2)
+                });
+            });
+
+            //  create button
+            display_element.append($('<button>', {
+                'id': 'next',
+                'class': 'sim',
+                'html': 'Submit Answer'
+            }));
+
+            // if prompt is set, show prompt
+            if (trial.prompt !== "") {
+                display_element.append(trial.prompt);
+            }
+
+            $("#next").click(function() {
+                var endTime = (new Date()).getTime();
+                var response_time = endTime - startTime;
+                sim_trial_complete = true;
+                var score = $("#slider").slider("value");
+                block.writeData($.extend({}, {
+                    "sim_score": score,
+                    "rt": response_time,
+                    "stimulus": trial.a_path,
+                    "stimulus_2": trial.b_path,
+                    "trial_type": "similarity",
+                    "trial_index": block.trial_idx
+                }, trial.data));
+                // goto next trial in block
+                display_element.html('');
+                setTimeout(function() {
+                    block.next();
+                }, trial.timing_post_trial);
+            });
+        }
 
         return plugin;
     })();
