f5bdd160953f234e5b652eae3b365b6372052d88
Josh de Leeuw <josh.deleeuw@gmail.com>
2014-06-24 14:28:56 -0400

    update to work with new centralized keyboard response method

--- plugins/jspsych-text.js
+++ plugins/jspsych-text.js
@@ -21,7 +21,7 @@
                 trials[i] = {};
                 trials[i].type = "text"; // must match plugin name
                 trials[i].text = params.text[i]; // text of all trials
-                trials[i].cont_key = params.cont_key || '13'; // keycode to press to advance screen, default is ENTER.
+                trials[i].cont_key = params.cont_key || []; // keycode to press to advance screen, default is all keys.
                 trials[i].timing_post_trial = (typeof params.timing_post_trial === 'undefined') ? 0 : params.timing_post_trial;
                 trials[i].data = (typeof params.data === 'undefined') ? {} : params.data[i];
             }
@@ -38,43 +38,12 @@
             // set the HTML of the display target to replaced_text.
             display_element.html(trial.text);
 
-            var startTime = (new Date()).getTime();
-
-            // it's possible that if the user is holding down the cont_key when
-            // they arrive on the page that they will advance as soon as the
-            // key is released. this prevents that from happening by requiring a
-            // full cycle on the page with a down and up event.
-            var cont_key_down = false;
-
-            // define a function that will advance to the next trial when the user presses
-            // the continue key.
-            var key_listener = function(e) {
-                if (e.which == trial.cont_key && cont_key_down) {
-                    save_data();
-                    $(document).unbind('keyup', key_listener); // remove the response function, so that it doesn't get triggered again.
-                    $(document).unbind('keydown', key_down_listener);
-                    display_element.html(''); // clear the display
-                    if (trial.timing_post_trial > 0) {
-                        setTimeout(function() {
-                            block.next();
-                        }, trial.timing_post_trial);
-                    }
-                    else {
-                        block.next();
-                    } // call block.next() to advance the experiment after a delay.
-                }
-            };
-
-            var key_down_listener = function(e) {
-                if (e.which == trial.cont_key) {
-                    cont_key_down = true;
-                }
-            };
-
-            var mouse_listener = function(e) {
-                save_data();
-                display_element.unbind('click', mouse_listener); // remove the response function, so that it doesn't get triggered again.
+            var after_response = function(info) {
+                    
                 display_element.html(''); // clear the display
+                
+                save_data(info.key, info.rt);
+                
                 if (trial.timing_post_trial > 0) {
                     setTimeout(function() {
                         block.next();
@@ -83,24 +52,33 @@
                 else {
                     block.next();
                 } // call block.next() to advance the experiment after a delay.
+                
+            };
+
+            var mouse_listener = function(e) {
+                
+                var rt = (new Date()).getTime() - start_time;
+                
+                display_element.unbind('click', mouse_listener); 
+                
+                after_response({key: 'mouse', rt: rt});
+                
             };
 
             // check if key is 'mouse'
             if (trial.cont_key == 'mouse') {
                 display_element.click(mouse_listener);
-            }
-            else {
-                // attach the response function to the html document.
-                $(document).keydown(key_down_listener);
-                $(document).keyup(key_listener);
+                var start_time = (new Date()).getTime();
+            } else {
+                core.getKeyboardResponse(after_key_response, trial.cont_key);
             }
 
-            var save_data = function() {
-                var rt = (new Date()).getTime() - startTime;
+            function save_data(rt, key) {
                 block.writeData($.extend({}, {
                     "trial_type": "text",
                     "trial_index": block.trial_idx,
-                    "rt": rt
+                    "rt": rt,
+                    "key_press", key
                 }, trial.data));
             };
         };
