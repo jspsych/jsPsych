3516456a526f1dd46819a5a039a28df0402c76cd
Josh de Leeuw <josh.deleeuw@gmail.com>
2014-12-16 16:02:20 -0500

    update to snap.svg dependency
    also fixes bizarre chrome issue that is probably a chrome bug.
    
    issue #78

--- plugins/jspsych-vsl-animate-occlusion.js
+++ plugins/jspsych-vsl-animate-occlusion.js
@@ -1,165 +1,163 @@
 /**
- * jsPsych plugin for showing animations that mimic the experiment described in
- *
- * Fiser, J., & Aslin, R. N. (2002). Statistical learning of higher-order
- * temporal structure from visual shape sequences. Journal of Experimental
- * Psychology: Learning, Memory, and Cognition, 28(3), 458.
- *
- * Josh de Leeuw
- *
- * documentation: docs.jspsych.org
- *
- */
+* jsPsych plugin for showing animations that mimic the experiment described in
+*
+* Fiser, J., & Aslin, R. N. (2002). Statistical learning of higher-order
+* temporal structure from visual shape sequences. Journal of Experimental
+* Psychology: Learning, Memory, and Cognition, 28(3), 458.
+*
+* Josh de Leeuw
+*
+* documentation: https://github.com/jodeleeuw/jsPsych/wiki/jspsych-vsl-animate-occlusion
+*
+*/
 
 (function($) {
-    jsPsych['vsl-animate-occlusion'] = (function() {
+  jsPsych['vsl-animate-occlusion'] = (function() {
 
-        var plugin = {};
+    var plugin = {};
 
-        plugin.create = function(params) {
+    plugin.create = function(params) {
 
-            var trials = new Array(1);
+      var trials = new Array(1);
 
-            trials[0] = {};
-            trials[0].type = "vsl-animate-occlusion";
-            trials[0].stims = params.stimuli;
-            trials[0].timing_cycle = params.timing_cycle || 1000;
-            trials[0].canvas_size = params.canvas_size || [400, 400];
-            trials[0].image_size = params.image_size || [100, 100];
-            trials[0].initial_direction = params.initial_direction || "left";
-            trials[0].occlude_center = (typeof params.occlude_center === 'undefined') ? true : params.occlude_center;
-            trials[0].choices = params.choices || []; // spacebar
-            trials[0].timing_pre_movement = (typeof params.timing_pre_movement === 'undefined') ? 500 : params.timing_pre_movement;
-            //trials[0].prompt = (typeof params.prompt === 'undefined') ? "" : params.prompt;
+      trials[0] = {};
+      trials[0].type = "vsl-animate-occlusion";
+      trials[0].stims = params.stimuli;
+      trials[0].timing_cycle = params.timing_cycle || 1000;
+      trials[0].canvas_size = params.canvas_size || [400, 400];
+      trials[0].image_size = params.image_size || [100, 100];
+      trials[0].initial_direction = params.initial_direction || "left";
+      trials[0].occlude_center = (typeof params.occlude_center === 'undefined') ? true : params.occlude_center;
+      trials[0].choices = params.choices || []; // spacebar
+      trials[0].timing_pre_movement = (typeof params.timing_pre_movement === 'undefined') ? 500 : params.timing_pre_movement;
+      //trials[0].prompt = (typeof params.prompt === 'undefined') ? "" : params.prompt;
 
-            return trials;
-        };
+      return trials;
+    };
 
-        plugin.trial = function(display_element, trial) {
+    plugin.trial = function(display_element, trial) {
 
-            // if any trial variables are functions
-            // this evaluates the function and replaces
-            // it with the output of the function
-            trial = jsPsych.pluginAPI.normalizeTrialVariables(trial);
+      // if any trial variables are functions
+      // this evaluates the function and replaces
+      // it with the output of the function
+      trial = jsPsych.pluginAPI.normalizeTrialVariables(trial);
 
-            // variable to keep track of timing info and responses
-            var start_time = 0;
-            var responses = [];
+      // variable to keep track of timing info and responses
+      var start_time = 0;
+      var responses = [];
 
-            var directions = [
-                [{
-                    params: {
-                        x: trial.canvas_size[0] - trial.image_size[0]
-                    },
-                    ms: trial.timing_cycle / 2
-                }, {
-                    params: {
-                        x: trial.canvas_size[0] / 2 - trial.image_size[0] / 2
-                    },
-                    ms: trial.timing_cycle / 2
-                }],
-                [{
-                    params: {
-                        x: 0
-                    },
-                    ms: trial.timing_cycle / 2
-                }, {
-                    params: {
-                        x: trial.canvas_size[0] / 2 - trial.image_size[0] / 2
-                    },
-                    ms: trial.timing_cycle / 2
-                }]
-            ];
+      var directions = [
+      [{
+        params: {
+          x: trial.canvas_size[0] - trial.image_size[0]
+        },
+        ms: trial.timing_cycle / 2
+      }, {
+        params: {
+          x: trial.canvas_size[0] / 2 - trial.image_size[0] / 2
+        },
+        ms: trial.timing_cycle / 2
+      }],
+      [{
+        params: {
+          x: 0
+        },
+        ms: trial.timing_cycle / 2
+      }, {
+        params: {
+          x: trial.canvas_size[0] / 2 - trial.image_size[0] / 2
+        },
+        ms: trial.timing_cycle / 2
+      }]
+      ];
 
-            var which_image = 0;
-            var next_direction = (trial.initial_direction == "right") ? 0 : 1;
+      var which_image = 0;
+      var next_direction = (trial.initial_direction == "right") ? 0 : 1;
 
-            function next_step() {
-                if (trial.stims.length == which_image) {
-                    endTrial();
-                }
-                else {
+      function next_step() {
+        if (trial.stims.length == which_image) {
+          endTrial();
+        }
+        else {
 
-                    var d = directions[next_direction];
-                    next_direction === 0 ? next_direction = 1 : next_direction = 0;
-                    var i = trial.stims[which_image];
-                    which_image++;
+          var d = directions[next_direction];
+          next_direction === 0 ? next_direction = 1 : next_direction = 0;
+          var i = trial.stims[which_image];
+          which_image++;
 
-                    eve.once("raphael.attr.src."+c.id, function(){
-                        c.animate(d[0].params, d[0].ms, function() {
-                            c.animate(d[1].params, d[1].ms, function() {
-                                next_step();
-                            });
-                        });
-                    });
-                    c.attr({
-                        src: i
-                    });
+          c.animate(d[0].params, d[0].ms, mina.linear, function() {
+            c.animate(d[1].params, d[1].ms, mina.linear, function() {
+              next_step();
+            });
+          });
 
-                    // start timer for this trial
-                    start_time = (new Date()).getTime();
-                }
-            }
+          c.attr({
+            href: i
+          });
 
-            display_element.append($("<div id='jspsych-vsl-animate-occlusion-raphaelCanvas'>", {
-                css: {
-                    width: trial.canvas_size[0] + "px",
-                    height: trial.canvas_size[1] + "px"
-                }
-            }));
+          // start timer for this trial
+          start_time = (new Date()).getTime();
+        }
+      }
 
-            var paper = Raphael("jspsych-vsl-animate-occlusion-raphaelCanvas", trial.canvas_size[0], trial.canvas_size[1]);
+      display_element.append($("<svg id='jspsych-vsl-animate-occlusion-canvas' width="+trial.canvas_size[0]+" height="+trial.canvas_size[1]+"></svg>"));
 
-            var c = paper.image(trial.stims[which_image], trial.canvas_size[0] / 2 - trial.image_size[0] / 2, trial.canvas_size[1] / 2 - trial.image_size[1] / 2, trial.image_size[0], trial.image_size[1]);
+      var paper = Snap("#jspsych-vsl-animate-occlusion-canvas");
 
-            if (trial.occlude_center) {
-                paper.rect((trial.canvas_size[0] / 2) - (trial.image_size[0] / 2), 0, trial.image_size[0], trial.canvas_size[1]).attr({
-                    fill: "#000"
-                });
-            }
+      var c = paper.image(trial.stims[which_image], trial.canvas_size[0] / 2 - trial.image_size[0] / 2, trial.canvas_size[1] / 2 - trial.image_size[1] / 2, trial.image_size[0], trial.image_size[1]).attr({
+        "id": 'jspsych-vsl-animate-occlusion-moving-image'
+      });
 
-            // add key listener
-            var after_response = function(info){
-                responses.push({
-                    key: info.key,
-                    stimulus: which_image - 1,
-                    rt: info.rt
-                });
-            }
+      document.getElementById('jspsych-vsl-animate-occlusion-moving-image').removeAttribute('preserveAspectRatio');
 
-            key_listener = jsPsych.pluginAPI.getKeyboardResponse(after_response, trial.choices, 'date', true);
+      if (trial.occlude_center) {
+        paper.rect((trial.canvas_size[0] / 2) - (trial.image_size[0] / 2), 0, trial.image_size[0], trial.canvas_size[1]).attr({
+          fill: "#000"
+        });
+      }
 
-            if (trial.timing_pre_movement > 0) {
-                setTimeout(function() {
-                    next_step();
-                }, trial.timing_pre_movement);
-            }
-            else {
-                next_step();
-            }
+      // add key listener
+      var after_response = function(info){
+        responses.push({
+          key: info.key,
+          stimulus: which_image - 1,
+          rt: info.rt
+        });
+      }
 
-            function endTrial() {
+      key_listener = jsPsych.pluginAPI.getKeyboardResponse(after_response, trial.choices, 'date', true);
 
-                display_element.html('');
+      if (trial.timing_pre_movement > 0) {
+        setTimeout(function() {
+          next_step();
+        }, trial.timing_pre_movement);
+      }
+      else {
+        next_step();
+      }
 
-                jsPsych.pluginAPI.cancelKeyboardResponse(key_listener);
+      function endTrial() {
 
-                jsPsych.data.write($.extend({}, {
-                    "stimuli": JSON.stringify(trial.stims),
-                    "responses": JSON.stringify(responses)
-                }, trial.data));
+        display_element.html('');
 
-                if (trial.timing_post_trial > 0) {
-                    setTimeout(function() {
-                        jsPsych.finishTrial();
-                    }, trial.timing_post_trial);
-                }
-                else {
-                    jsPsych.finishTrial();
-                }
-            }
-        };
+        jsPsych.pluginAPI.cancelKeyboardResponse(key_listener);
 
-        return plugin;
-    })();
+        jsPsych.data.write($.extend({}, {
+          "stimuli": JSON.stringify(trial.stims),
+          "responses": JSON.stringify(responses)
+        }, trial.data));
+
+        if (trial.timing_post_trial > 0) {
+          setTimeout(function() {
+            jsPsych.finishTrial();
+          }, trial.timing_post_trial);
+        }
+        else {
+          jsPsych.finishTrial();
+        }
+      }
+    };
+
+    return plugin;
+  })();
 })(jQuery);
