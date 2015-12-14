e6092d685fcf7792c430af1785ae8184013151b0
Josh de Leeuw <josh.deleeuw@gmail.com>
2015-12-14 16:57:14 -0500

    formatting

--- plugins/template/jspsych-plugin-template.js
+++ plugins/template/jspsych-plugin-template.js
@@ -2,31 +2,31 @@
  * Example plugin template
  */
 
-	jsPsych["PLUGIN-NAME"] = (function(){
+jsPsych["PLUGIN-NAME"] = (function() {
 
-		var plugin = {};
+  var plugin = {};
 
-		plugin.trial = function(display_element, trial) {
+  plugin.trial = function(display_element, trial) {
 
-      // set default values for parameters
-      trial.parameter = trial.parameter || 'default value';
+    // set default values for parameters
+    trial.parameter = trial.parameter || 'default value';
 
-			// allow variables as functions
-			// this allows any trial variable to be specified as a function
-			// that will be evaluated when the trial runs. this allows users
-			// to dynamically adjust the contents of a trial as a result
-			// of other trials, among other uses. you can leave this out,
-			// but in general it should be included
-      trial = jsPsych.evaluateFunctionParameters(trial);
+    // allow variables as functions
+    // this allows any trial variable to be specified as a function
+    // that will be evaluated when the trial runs. this allows users
+    // to dynamically adjust the contents of a trial as a result
+    // of other trials, among other uses. you can leave this out,
+    // but in general it should be included
+    trial = jsPsych.evaluateFunctionParameters(trial);
 
-			// data saving
-			var trial_data = {
-			    parameter_name: 'parameter value'
-			};
+    // data saving
+    var trial_data = {
+      parameter_name: 'parameter value'
+    };
 
-      // end trial
-			jsPsych.finishTrial(trial_data);
-		};
+    // end trial
+    jsPsych.finishTrial(trial_data);
+  };
 
-		return plugin;
-	})();
+  return plugin;
+})();
