5f40e184b925c30d124ef0d4e27ed816baeb98a5
Josh de Leeuw <josh.deleeuw@gmail.com>
2016-12-10 11:03:53 -0500

    basic testing with jest working

--- tests/plugin-single-stim.test.js
+++ tests/plugin-single-stim.test.js
@@ -1,5 +1,32 @@
-require('../jspsych.js')
+require('../jspsych.js');
+require('../plugins/jspsych-single-stim.js');
 
 test('jsPsych should be in the window object', function(){
   expect(typeof window.jsPsych).not.toBe('undefined');
 });
+
+test('the single-stim plugin should be loaded', function(){
+  expect(typeof window.jsPsych.plugins['single-stim']).not.toBe('undefined');
+})
+
+var trial = {
+  type: 'single-stim',
+  stimulus: '<p>Hello</p>',
+  is_html: true
+}
+
+jsPsych.init({
+  timeline: [trial]
+});
+
+test('HMTL stimulus should display', function(){
+  var display_element = jsPsych.getDisplayElement();
+  expect(display_element.innerHTML).toBe('<div id="jspsych-single-stim-stimulus"><p>Hello</p></div>');
+});
+
+test('Display should clear after keypress', function(){
+  document.dispatchEvent(new KeyboardEvent('keydown', {keyCode: 32}));
+  document.dispatchEvent(new KeyboardEvent('keyup', {keyCode: 32}));
+  var display_element = jsPsych.getDisplayElement();
+  expect(display_element.innerHTML).toBe('');
+})
