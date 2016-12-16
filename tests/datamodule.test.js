3d1c2b9ac9804a02d7cf2fc9aa8ea107f6b597cf
Josh de Leeuw <josh.deleeuw@gmail.com>
2016-12-16 11:24:30 -0500

    add test

--- tests/datamodule.test.js
+++ tests/datamodule.test.js
@@ -1,6 +1,20 @@
 require('../jspsych.js');
 require('../plugins/jspsych-text.js');
 
+describe('Basic data recording', function(){
+  test('should be able to get rt after running experiment', function(){
+    var timeline = [
+      {type: 'text', text:'hello'}
+    ];
+    jsPsych.init({timeline:timeline});
+    // click through first trial
+    document.dispatchEvent(new KeyboardEvent('keydown', {keyCode: 32}));
+    document.dispatchEvent(new KeyboardEvent('keyup', {keyCode: 32}));
+    // check if data contains rt
+    expect(jsPsych.data.getData().select('rt').count()).toBe(1);
+  })
+})
+
 describe('#addProperties', function(){
   test('should add data to all trials if called before experiment starts', function(){
     var timeline = [
@@ -12,7 +26,6 @@ describe('#addProperties', function(){
     document.dispatchEvent(new KeyboardEvent('keydown', {keyCode: 32}));
     document.dispatchEvent(new KeyboardEvent('keyup', {keyCode: 32}));
     // check if data contains testprop
-    expect(jsPsych.data.getData().select('rt').count()).toBe(1);
     expect(jsPsych.data.getData().select('testprop').count()).toBe(1);
   });
 })
