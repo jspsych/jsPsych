b01f4039f3ae72157b5489a4a3b9a403fd51c641
Josh de Leeuw <josh.deleeuw@gmail.com>
2017-02-02 16:47:23 -0500

    add tests for data parameter

---
+++ tests/jsPsych.data/dataparameter.test.js
@@ -0,0 +1,109 @@
+const root = '../../';
+
+describe('The data parameter', function(){
+  test('should record data to a trial', function(){
+
+    require(root + 'jspsych.js');
+    require(root + 'plugins/jspsych-text.js');
+
+    return (new Promise(function(resolve, reject){
+
+      var key_data = null;
+
+      var trial = {
+        type: 'text',
+        text: 'hello',
+        data: {added: true},
+      }
+
+      jsPsych.init({
+        timeline: [trial],
+        on_finish: function() {
+          var d = jsPsych.data.get().values()[0].added;
+          resolve(d);
+        }
+      });
+
+      document.dispatchEvent(new KeyboardEvent('keydown', {keyCode: 32}));
+      document.dispatchEvent(new KeyboardEvent('keyup', {keyCode: 32}));
+
+      //resolve();
+    })).then(function(data) { expect(data).toBe(true) });
+  });
+
+  test('should record data to all nested trials', function(){
+
+    require(root + 'jspsych.js');
+    require(root + 'plugins/jspsych-text.js');
+
+    return (new Promise(function(resolve, reject){
+
+      var key_data = null;
+
+      var trial = {
+        type: 'text',
+        timeline: [
+          {text: 'a'},
+          {text: 'b'}
+        ],
+        data: {added: true},
+      }
+
+      jsPsych.init({
+        timeline: [trial],
+        on_finish: function() {
+          var d = jsPsych.data.get().filter({added: true}).count();
+          resolve(d);
+        }
+      });
+
+      document.dispatchEvent(new KeyboardEvent('keydown', {keyCode: 32}));
+      document.dispatchEvent(new KeyboardEvent('keyup', {keyCode: 32}));
+
+      document.dispatchEvent(new KeyboardEvent('keydown', {keyCode: 32}));
+      document.dispatchEvent(new KeyboardEvent('keyup', {keyCode: 32}));
+
+      //resolve();
+    })).then(function(data) { expect(data).toBe(2) });
+  });
+
+  test('should record data to all nested trials with timeline variables', function(){
+
+    require(root + 'jspsych.js');
+    require(root + 'plugins/jspsych-text.js');
+
+    return (new Promise(function(resolve, reject){
+
+      var key_data = null;
+
+      var vars = [
+        {text: 'a'},
+        {text: 'b'}
+      ];
+
+      var trial = {
+        timeline: [
+          {type: 'text', text: jsPsych.timelineVariable('text')}
+        ],
+        timeline_variables: vars,
+        data: {added: true},
+      }
+
+      jsPsych.init({
+        timeline: [trial],
+        on_finish: function() {
+          var d = jsPsych.data.get().filter({added: true}).count();
+          resolve(d);
+        }
+      });
+
+      document.dispatchEvent(new KeyboardEvent('keydown', {keyCode: 32}));
+      document.dispatchEvent(new KeyboardEvent('keyup', {keyCode: 32}));
+
+      document.dispatchEvent(new KeyboardEvent('keydown', {keyCode: 32}));
+      document.dispatchEvent(new KeyboardEvent('keyup', {keyCode: 32}));
+
+      //resolve();
+    })).then(function(data) { expect(data).toBe(2) });
+  });
+});
