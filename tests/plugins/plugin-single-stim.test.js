210dc8b8343a735e07100a390a187ea674c48801
Josh de Leeuw <josh.deleeuw@gmail.com>
2017-03-19 21:44:41 -0400

    flesh out testing suite framework for plugins #351

--- tests/plugins/plugin-single-stim.test.js
+++ tests/plugins/plugin-single-stim.test.js
@@ -1,34 +1,56 @@
 const root = '../../';
 
-require(root + 'jspsych.js');
-require(root + 'plugins/jspsych-single-stim.js');
+describe('single-stim plugin', function(){
 
-test('jsPsych should be in the window object', function(){
-  expect(typeof window.jsPsych).not.toBe('undefined');
+  beforeEach(function(){
+    require(root + 'jspsych.js');
+    require(root + 'plugins/jspsych-single-stim.js');
+  });
+
+  test('loads correctly', function(){
+    expect(typeof window.jsPsych.plugins['single-stim']).not.toBe('undefined');
+  });
+
+  test('displays image by default', function(){
+    var trial = {
+      type: 'single-stim',
+      stimulus: '../media/blue.png'
+    }
+
+    jsPsych.init({
+      timeline: [trial]
+    });
+
+    expect(jsPsych.getDisplayElement().innerHTML).toBe('<img src="../media/blue.png" id="jspsych-single-stim-stimulus">');
+  });
+
+  test('displays html when is_html is true', function(){
+    var trial = {
+      type: 'single-stim',
+      stimulus: '<p>hello</p>',
+      is_html: true
+    }
+
+    jsPsych.init({
+      timeline: [trial]
+    });
+
+    expect(jsPsych.getDisplayElement().innerHTML).toBe('<div id="jspsych-single-stim-stimulus"><p>hello</p></div>');
+  });
+
+  test('display should clear after key press', function(){
+    var trial = {
+      type: 'single-stim',
+      stimulus: '<p>hello</p>'
+    }
+
+    jsPsych.init({
+      timeline: [trial]
+    });
+
+    document.dispatchEvent(new KeyboardEvent('keydown', {keyCode: 32}));
+    document.dispatchEvent(new KeyboardEvent('keyup', {keyCode: 32}));
+
+    expect(jsPsych.getDisplayElement().innerHTML).toBe('');
+  });
 });
-
-test('the single-stim plugin should be loaded', function(){
-  expect(typeof window.jsPsych.plugins['single-stim']).not.toBe('undefined');
-})
-
-var trial = {
-  type: 'single-stim',
-  stimulus: '<p>Hello</p>',
-  is_html: true
-}
-
-jsPsych.init({
-  timeline: [trial]
-});
-
-test('HMTL stimulus should display', function(){
-  var display_element = jsPsych.getDisplayElement();
-  expect(display_element.innerHTML).toBe('<div id="jspsych-single-stim-stimulus"><p>Hello</p></div>');
-});
-
-test('Display should clear after keypress', function(){
-  document.dispatchEvent(new KeyboardEvent('keydown', {keyCode: 32}));
-  document.dispatchEvent(new KeyboardEvent('keyup', {keyCode: 32}));
-  var display_element = jsPsych.getDisplayElement();
-  expect(display_element.innerHTML).toBe('');
-})
