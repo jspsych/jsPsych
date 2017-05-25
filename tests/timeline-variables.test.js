90a17b82ec669329bfa0b24a0cbfe224c0832bb7
Josh de Leeuw <josh.deleeuw@gmail.com>
2017-05-24 23:28:11 -0400

    add timeline variables test shell

---
+++ tests/timeline-variables.test.js
@@ -0,0 +1,24 @@
+const root = '../';
+
+beforeEach(function(){
+  require(root + 'jspsych.js');
+  require(root + 'plugins/jspsych-text.js');
+});
+
+describe('randomize order', function(){
+  test('holder', function(){
+    expect(true).toBe(true);
+  });
+});
+
+describe('repetitons', function(){
+  test('holder', function(){
+    expect(true).toBe(true);
+  });
+});
+
+describe('sampling', function(){
+  test('holder', function(){
+    expect(true).toBe(true);
+  });
+});
