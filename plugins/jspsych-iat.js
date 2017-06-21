ed3250d48f33d82236ac41f41710d2a6823c0019
Josh de Leeuw <josh.deleeuw@gmail.com>
2017-06-21 09:10:44 -0400

    set default for false correctly

--- plugins/jspsych-iat.js
+++ plugins/jspsych-iat.js
@@ -1,6 +1,6 @@
 /**
  * jspsych-iat
- * Kristin Diep 
+ * Kristin Diep
  *
  * plugin for displaying a stimulus and getting a keyboard response
  *
@@ -15,7 +15,7 @@
 
   jsPsych.pluginAPI.registerPreload('iat', 'stimulus', 'image', function(t){return !t.is_html || t.is_html == 'undefined'});
 
-  
+
   plugin.trial = function(display_element, trial) {
 
     var plugin_id_name = "jspsych-iat";
@@ -25,66 +25,61 @@
       return arr.join(separator = '-');
     }
 
-
     // if any trial variables are functions
     // this evaluates the function and replaces
     // it with the output of the function
     trial = jsPsych.pluginAPI.evaluateFunctionParameters(trial);
 
     // set default values for the parameters
-    trial.display_feedback = trial.display_feedback || false;
+    trial.display_feedback = typeof trial.display_feedback == 'undefined' ? false : trial.display_feedback;
     trial.image_when_wrong = trial.image_when_wrong || 'undefined';
     trial.wrong_image_name = trial.wrong_image_name || 'wrong image';
     trial.left_category_key = trial.left_category_key || 'E';
     trial.right_category_key = trial.right_category_key || 'I';
     trial.left_category_label = trial.left_category_label || ['left'];
-    trial.right_category_label = trial.right_category_label || ['right']; 
+    trial.right_category_label = trial.right_category_label || ['right'];
     trial.stim_key_association = trial.stim_key_association || 'undefined';
     trial.response_ends_trial = (typeof trial.response_ends_trial == 'undefined') ? true : trial.response_ends_trial;
     trial.timing_response = trial.timing_response || -1;
-    trial.key_to_move_forward = trial.key_to_move_forward || [jsPsych.ALL_KEYS];
-    trial.prompt = trial.prompt || "";
+    trial.key_to_move_forward = trial.key_to_move_forward || jsPsych.ALL_KEYS;
 
-    //Get keys to continue and put them in a string 
-    var i;
+    //Get keys to continue and put them in a string
     var keysToContinue = "";
     var lastKey = trial.key_to_move_forward.length - 1;
-    for(i = 0; i < lastKey; i++) {
+    for(var i = 0; i < lastKey; i++) {
       keysToContinue += trial.key_to_move_forward[i] + ", ";
     }
-  
 
-    var html_str = " ";
-    //html_str += "<div class='main_layout' style='position: relative; height: 100vh; width: 100vh' id='jspsych-iat-stim'>";
+    var html_str = "";
 
     if (!trial.is_html) {
       html_str += "<div style='position: absolute; height: 20%; width: 100%; margin-left: auto; margin-right: auto; top: 42%; left: 0; right: 0'><img src='"+trial.stimulus+"' id='jspsych-iat-stim'></img></div>";
     } else {
       html_str += "<div style='position: absolute; height: 20%; width: 100%; margin-left: auto; margin-right: auto; top: 42%; left: 0; right: 0'><p id='jspsych-iat-stim'>" + trial.stimulus + "</p></div>";
-    } 
+    }
 
     html_str += "<div id='trial_left_align' style='position: absolute; top: 18%; left: 20%'>";
 
     if(trial.left_category_label.length == 1) {
-      html_str += "<p>Press " + trial.left_category_key + " for:<br> " + 
+      html_str += "<p>Press " + trial.left_category_key + " for:<br> " +
       trial.left_category_label[0].bold() + "</p></div>";
     } else {
-      html_str += "<p>Press " + trial.left_category_key + " for:<br> " + 
+      html_str += "<p>Press " + trial.left_category_key + " for:<br> " +
       trial.left_category_label[0].bold() + "<br>" + "or<br>" +
       trial.left_category_label[1].bold() + "</p></div>";
     }
-    
+
     html_str += "<div id='trial_right_align' style='position: absolute; top: 18%; right: 20%'>";
 
     if(trial.right_category_label.length == 1) {
-      html_str += "<p>Press " + trial.right_category_key + " for:<br> " + 
+      html_str += "<p>Press " + trial.right_category_key + " for:<br> " +
       trial.right_category_label[0].bold() + '</p></div>';
     } else {
-      html_str += "<p>Press " + trial.right_category_key + " for:<br> " + 
-      trial.right_category_label[0].bold() + "<br>" + "or<br>" + 
+      html_str += "<p>Press " + trial.right_category_key + " for:<br> " +
+      trial.right_category_label[0].bold() + "<br>" + "or<br>" +
       trial.right_category_label[1].bold() + "</p></div>";
     }
-    
+
     html_str += "<div id='wrongImgID' style='position:relative; top: 300px; margin-left: auto; margin-right: auto; left: 0; right: 0'>";
 
     if(!trial.response_ends_trial && trial.display_feedback == true) {
@@ -100,7 +95,7 @@
     if(trial.response_ends_trial && trial.display_feedback == true) {
       html_str += "<div id='wrongImgContainer' style='visibility: hidden; position: absolute; top: -75px; margin-left: auto; margin-right: auto; left: 0; right: 0'><img src='" + trial.image_when_wrong + "' id='wrongImgID'></img></div>";
       var wImg = document.getElementById("wrongImgContainer");
-      //wImg.style.visibility = "hidden"; 
+      //wImg.style.visibility = "hidden";
 
       if(trial.key_to_move_forward.length == 1) {
         if(trial.key_to_move_forward[0] == "other key") {
@@ -111,7 +106,7 @@
         html_str += "<p>If you press the wrong key, a " + trial.wrong_image_name + " will appear. Press " + trial.key_to_move_forward[0] + " to continue.</p>";
       }
       } else {
-        html_str += "<p>If you press the wrong key, a " + trial.wrong_image_name + " will appear. Press " + 
+        html_str += "<p>If you press the wrong key, a " + trial.wrong_image_name + " will appear. Press " +
         keysToContinue + " " + trial.key_to_move_forward[lastKey] + " to continue.</p>";
       }
     } else if (trial.response_ends_trial && trial.display_feedback != true) {
@@ -125,20 +120,15 @@
         html_str += "<p>If you press the wrong key, press " + trial.key_to_move_forward[0] + " to continue.</p>";
         }
         } else {
-        html_str += "<p>If you press the wrong key, press " + 
+        html_str += "<p>If you press the wrong key, press " +
         keysToContinue + " " + trial.key_to_move_forward[lastKey] + " to continue.</p>";
       }
     }
 
     html_str += "</div>";
-    
-    display_element.innerHTML += html_str;
 
+    display_element.innerHTML = html_str;
 
-    //show prompt if there is one
-    if (trial.prompt !== "") {
-      display_element.innerHTML += trial.prompt;
-    }
 
     // store response
     var response = {
@@ -166,9 +156,6 @@
         "correct": response.correct
       };
 
-      jsPsych.data.write(trial_data);
-      jsPsych.data.addDataToLastTrial(trial_data);
-
       // clears the display
       display_element.innerHTML = '';
 
@@ -176,10 +163,8 @@
       jsPsych.finishTrial(trial_data);
     };
 
-
-    var leftKeyCode = jsPsych.pluginAPI.convertKeyCharacterToKeyCode(trial.left_category_key); 
+    var leftKeyCode = jsPsych.pluginAPI.convertKeyCharacterToKeyCode(trial.left_category_key);
     var rightKeyCode = jsPsych.pluginAPI.convertKeyCharacterToKeyCode(trial.right_category_key);
-    
 
     // function to handle responses by the subject
     var after_response = function(info) {
@@ -192,7 +177,7 @@
       if (response.key == -1 ) {
         response = info;
       }
-      
+
       if(trial.stim_key_association == "right") {
         if(response.rt > -1 && response.key == rightKeyCode) {
           response.correct = true;
@@ -257,7 +242,7 @@
             // end_trial();
           }
         }
-      } 
+      }
     };
 
     // start the response listener
@@ -270,14 +255,13 @@
         allow_held_key: false
       });
     }
-    
 
     // end trial if time limit is set
     if (trial.timing_response > 0 && trial.response_ends_trial != true) {
       jsPsych.pluginAPI.setTimeout(function() {
         end_trial();
       }, trial.timing_response);
-    } 
+    }
 
   };
 
