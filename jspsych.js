bc44f2b1fb23cc1ec096aeca12b839f01d586f94
Teon L Brooks <teon.brooks@gmail.com>
2017-11-06 13:22:32 -0800

    make jspsych cjs and amd compatible

--- jspsych.js
+++ jspsych.js
@@ -5,7 +5,16 @@
  * documentation: docs.jspsych.org
  *
  **/
-window.jsPsych = (function() {
+
+(function (root, factory) {
+  if(typeof define === "function" && define.amd) {
+     define([], factory);
+  } else if(typeof module === "object" && module.exports) {
+     module.exports = factory();
+  } else {
+     root.jsPsych = factory();
+  }
+}(this, function jsPsych() {
 
   var core = {};
 
@@ -954,7 +963,7 @@ window.jsPsych = (function() {
   document.documentElement.setAttribute('jspsych', 'present');
 
   return core;
-})();
+}));
 
 jsPsych.plugins = (function() {
 
