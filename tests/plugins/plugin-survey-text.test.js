f2319fec53ed90617530d7b1ebbef4613ba20a38
Josh de Leeuw <josh.deleeuw@gmail.com>
2018-03-11 17:06:28 -0400

    add a test case for survey-text defaults

--- tests/plugins/plugin-survey-text.test.js
+++ tests/plugins/plugin-survey-text.test.js
@@ -1,4 +1,5 @@
 const root = '../../';
+const utils = require('../testing-utils.js');
 
 jest.useFakeTimers();
 
@@ -11,6 +12,25 @@ describe('survey-text plugin', function(){
 
 	test('loads correctly', function(){
 		expect(typeof window.jsPsych.plugins['survey-text']).not.toBe('undefined');
+	});
+
+	test('default parameters work correctly', function(){
+		var trial = {
+			type: 'survey-text',
+			questions: [{prompt: "How old are you?"}, {prompt: "Where were you born?"}]
+		}
+
+		jsPsych.init({
+			timeline: [trial]
+		});
+
+		expect(jsPsych.getDisplayElement().querySelectorAll('p.jspsych-survey-text').length).toBe(2);
+		expect(jsPsych.getDisplayElement().querySelector('input[name="#jspsych-survey-text-response-0"]').size).toBe(40);
+		expect(jsPsych.getDisplayElement().querySelector('input[name="#jspsych-survey-text-response-1"]').size).toBe(40);
+
+		utils.clickTarget(document.querySelector('#jspsych-survey-text-next'));
+
+		expect(jsPsych.getDisplayElement().innerHTML).toBe('');
 	});
 
 });
