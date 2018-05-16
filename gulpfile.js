542fc9efbb407864ff1d8e54fc3d822d42fefba6
Rob Wilkinson <wilkinson.robert.a@gmail.com>
2018-05-16 10:08:59 -0700

    prettified gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,33 +5,33 @@ const path =require('path')
 
 gulp.task('umd', function() {
   gulp.src('lib/plugins/*.js')
-    .pipe(umd({
-       namespace: function(file) {
-            return 'jsPsych.plugins["' + path.basename(file.path).replace('jspsych-', '').replace('.js','') + '"]';
-        },
-    	dependencies: function() {
-    		return [
-	    		{
-                        name: 'jspsych',
-                        amd: 'jspsych',
-                        cjs: 'jspsych',
-                        global: 'jsPsych',
-                        param: 'jsPsych'
-	            }
-    		]
-    	},
-        template: path.resolve(__dirname, 'templates', 'jsPsychPlugin.js')
-    }))
-    .pipe(gulp.dest('build/plugins'));
+  .pipe(umd({
+     namespace: function(file) {
+        return 'jsPsych.plugins["' + path.basename(file.path).replace('jspsych-', '').replace('.js','') + '"]';
+    },
+    dependencies: function() {
+      return [
+      {
+        name: 'jspsych',
+        amd: 'jspsych',
+        cjs: 'jspsych',
+        global: 'jsPsych',
+        param: 'jsPsych'
+    }
+    ]
+},
+template: path.resolve(__dirname, 'templates', 'jsPsychPlugin.js')
+}))
+  .pipe(gulp.dest('build/plugins'));
   gulp.src('lib/jspsych.js')
-    .pipe(umd({
-          exports: function(file) {
-                return 'jsPsych';
-            },
-        namespace:function() { 
-            return 'jsPsych'
-        },
-        template: patterns.returnExportsGlobal.path
-    }))
-    .pipe(gulp.dest('build'));
+  .pipe(umd({
+      exports: function(file) {
+        return 'jsPsych';
+    },
+    namespace:function() { 
+        return 'jsPsych'
+    },
+    template: patterns.returnExportsGlobal.path
+}))
+  .pipe(gulp.dest('build'));
 });
