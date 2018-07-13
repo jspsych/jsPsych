72c84130c58b17d9fc5d17ce072278cb779c170f
Philipp Sprengholz <philipp.sprengholz@outlook.com>
2018-07-13 23:15:38 +0200

    Refined the cloze structure and added both example and doc files

--- plugins/jspsych-cloze.js
+++ plugins/jspsych-cloze.js
@@ -14,14 +14,26 @@ jsPsych.plugins['cloze'] = (function () {
                 type: jsPsych.plugins.parameterType.STRING,
                 pretty_name: 'Cloze text',
                 default: undefined,
-                description: 'Cloze text with %input to check%'
+                description: 'Cloze text with %input to check% (surrounded by percentage signs)'
+            },
+            allow_mistakes: {
+                type: jsPsych.plugins.parameterType.BOOL,
+                pretty_name: 'Allow mistakes',
+                default: true,
+                description: 'Allow finishing the trial while answers are not correct'
             },
             mistake_fn: {
                 type: jsPsych.plugins.parameterType.FUNCTION,
                 pretty_name: 'Mistake function',
                 default: function () {},
-                description: 'Function called in case of wrong answers'
-            }
+                description: 'Function called in case of wrong answers and if allow_mistakes is set to TRUE'
+            },
+            button_text: {
+                type: jsPsych.plugins.parameterType.STRING,
+                pretty_name: 'Button text',
+                default: 'OK',
+                description: 'The text of the button to finish the trial'
+            },
         }
     };
 
@@ -41,7 +53,7 @@ jsPsych.plugins['cloze'] = (function () {
             else
             {
                 solutions.push(elements[i].trim());
-                html += '<input type="text" id="input'+(solutions.length-1)+'" value="" placeholder="?">';
+                html += '<input type="text" id="input'+(solutions.length-1)+'" value="">';
             }
         }
         html += '</div>';
@@ -55,45 +67,58 @@ jsPsych.plugins['cloze'] = (function () {
       
         var check = function() {
 
-            answersCorrect = true;
+            answers_correct = true;
             attempts.push([]);
             for (i=0; i<solutions.length; i++)
             {
-                field = $('#input'+i);
-                answer = field.val().trim();
+                field = document.getElementById('input'+i);
+                answer = field.value.trim();
+                //answer = field.val().trim();
                 attempts[attempts.length-1].push(answer);
                 
                 if (answer.toLowerCase() !== solutions[i].toLowerCase())
                 {
-                    field.css('color', 'red');
-                    answersCorrect = false;
+                    field.style.color = 'red';
+                    answers_correct = false;
                 }
                 else
                 {
-                    field.css('color', 'black');
+                    field.style.color = 'black';
                 }
             }   
             
             var trial_data = {
                 'solution' : solutions,
-                'attempts' : attempts
+                'attempts' : attempts,
+                'answer_correct' : answers_correct
             };
             
-            if(answersCorrect)
+
+            if(trial.allow_mistakes)
             {
                 display_element.innerHTML = '';
                 jsPsych.finishTrial(trial_data); 
             }
             else
             {
-                trial.mistake_fn();
+                if(answers_correct)
+                {
+                    display_element.innerHTML = '';
+                    jsPsych.finishTrial(trial_data); 
+                }
+            else
+                {
+                    trial.mistake_fn();
+                }
             }
+
+            
             
         };
         
-        display_element.innerHTML += '<br><button class="navbutton" type="button" id="finishcloze">OK</button>';
+        display_element.innerHTML += '<br><button class="jspsych-html-button-response-button" type="button" id="finish_cloze_button">'+trial.button_text+'</button>';
         
-        display_element.querySelector('#finishcloze').addEventListener('click', check);
+        display_element.querySelector('#finish_cloze_button').addEventListener('click', check);
     };
 
     return plugin;
