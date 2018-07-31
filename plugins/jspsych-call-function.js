0b5b3004de87715f2b4f40b75b45e37318638db8
Josh de Leeuw <josh.deleeuw@gmail.com>
2018-07-31 10:48:54 -0400

    implement async option for call-function plugin #552

--- plugins/jspsych-call-function.js
+++ plugins/jspsych-call-function.js
@@ -21,18 +21,37 @@ jsPsych.plugins['call-function'] = (function() {
         default: undefined,
         description: 'Function to call'
       },
+      async: {
+        type: jsPsych.plugins.parameterType.BOOL,
+        pretty_name: 'Asynchronous',
+        default: false,
+        description: 'Is the function call asynchronous?'
+      }
     }
   }
 
   plugin.trial = function(display_element, trial) {
     trial.post_trial_gap = 0;
-    var return_val = trial.func();
+    var return_val;
 
-    var trial_data = {
-      value: return_val
-    };
-
-    jsPsych.finishTrial(trial_data);
+    if(trial.async){
+      var done = function(data){
+        return_val = data;
+        end_trial();    
+      }
+      trial.func(done);
+    } else {
+      return_val = trial.func();
+      end_trial();
+    }
+    
+    function end_trial(){
+      var trial_data = {
+        value: return_val
+      };
+  
+      jsPsych.finishTrial(trial_data);
+    }
   };
 
   return plugin;
