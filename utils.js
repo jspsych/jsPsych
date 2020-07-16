63608b46d7e7ffbbc5e9d4349c1b6dec665e267f
Kxnr <kahana-sysadmin@sas.upenn.edu>
2020-07-16 19:40:17 -0400

    butchered the attention check

--- utils.js
+++ utils.js
@@ -8,17 +8,8 @@ var jsPsychUtils = {
         let question_1 = {
             type: 'html-button-response',
             choices: ["<5 years", "5-15 years", "15-25 years", "25+ years"],
-            stimulus: "<p id='inst-justified'>According to Wikipedia, Memory is the faculty of the brain, " +
-                    "by which data or information is encoded, stored, and retrieved " + 
-                    "when needed. It is the retention of information over time for " + 
-                    "not be remembered, it would be impossible for language, " +
-                    "relationships, or personal identity to develop. Please answer " +
-                    "the question below honestly, but for the question on the next " +
-                    "page, pick 'never.' Psychologists most generally classify memory " +
-                    "as Declarative or Implicit, where Declarative memories " +
-                    "include memories for people, places, and events in our lives. " +
-                    "It is this type of memory that is most often intended when " +
-                    "speaking about memory.<p><p id='inst'>How old is your oldest memory?</p>",
+            stimulus: "<p id='inst-justified'>According to Wikipedia, Memory is the faculty of the brain by which data or information is encoded, stored, and retrieved when needed. It is the retention of information over time for the purpose of influencing future action. If past events could not be remembered, it would be impossible for language, relationships, or personal identity to develop. Please answer the question below honestly, but for the next question, pick 'never.' Psychologists most generally classify memory as Declarative or Implicit, where Declarative memories include memories for people, places, and events in our lives. It is this type of memory that is most often intended when speaking about memory.<p>" +
+            "<p id='inst'>How old is your oldest memory?</p>",
         };
 
         let question_2 = {
