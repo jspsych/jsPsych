c7b3762c0a51539e42462391201a5ba12fdcbf0c
Becky Gilbert <beckyannegilbert@gmail.com>
2020-12-03 09:18:07 -0800

    fix registerPreload test

--- tests/plugins/plugin-video-slider-response.test.js
+++ tests/plugins/plugin-video-slider-response.test.js
@@ -6,16 +6,18 @@ describe('video-slider-response plugin', function(){
 
 	beforeEach(function(){
 		require(root + 'jspsych.js');
-		require(root + 'plugins/jspsych-video-slider-response.js');
+		// don't load plugin here - need to spy on registerPreload before its called
 	});
 
 	test('loads correctly', function(){
+		require(root + 'plugins/jspsych-video-slider-response.js');
 		expect(typeof window.jsPsych.plugins['video-slider-response']).not.toBe('undefined');
 	});
 
 	test('video preloading initiates automatically', function(){
 		const preload_spy = jest.spyOn(jsPsych.pluginAPI, 'registerPreload');
 		const console_spy = jest.spyOn(console, 'warn');
+		require(root + 'plugins/jspsych-video-slider-response.js');
 		var trial = {
 			type: 'video-slider-response',
 			stimulus: [root + 'tests/media/sample_video.mp4']
@@ -23,7 +25,7 @@ describe('video-slider-response plugin', function(){
 		jsPsych.init({
 			timeline: [trial]
 		});
-		//expect(preload_spy).toHaveBeenCalled(); // NOT WORKING
+		expect(preload_spy).toHaveBeenCalled();
 		expect(console_spy).not.toHaveBeenCalled();
 		preload_spy.mockRestore();
 		console_spy.mockRestore();
