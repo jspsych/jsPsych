3a08b15fc9bd41362b9810d1d038cdfa591d9f34
Josh de Leeuw <josh.deleeuw@gmail.com>
2021-01-06 18:54:28 -0500

    start implementing plugin, didn't get far yet

---
+++ plugins/jspsych-preload.js
@@ -0,0 +1,56 @@
+/**
+ * jspsych-preload
+ * documentation: docs.jspsych.org
+ *
+ **/
+
+jsPsych.plugins['preload'] = (function() {
+
+    var plugin = {};
+  
+    plugin.info = {
+      name: 'preload',
+      description: '',
+      parameters: {
+        auto_preload: {
+          type: jsPsych.plugins.parameterType.BOOL,
+          default: false
+        },
+        images: {
+          type: jsPsych.plugins.parameterType.STRING,
+          default: []
+        },
+        audio: {
+          type: jsPsych.plugins.parameterType.STRING,
+          default: []
+        },
+        video: {
+          type: jsPsych.plugins.parameterType.STRING,
+          default: []
+        },
+        show_progress_bar: {
+          type: jsPsych.plugins.parameterType.BOOL,
+          default: true,
+        },
+        show_detailed_errors: {
+          type: jsPsych.plugins.parameterType.BOOL,
+          default: false
+        },
+      }
+    }
+  
+    plugin.trial = function(display_element, trial) {
+      
+      
+      function end_trial(){
+        var trial_data = {
+          value: return_val
+        };
+    
+        jsPsych.finishTrial(trial_data);
+      }
+    };
+  
+    return plugin;
+  })();
+  
