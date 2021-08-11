efd0c4134c510a8b573a1fa988294ca4e06ec53e
Josh de Leeuw <josh.deleeuw@gmail.com>
2021-08-11 10:54:06 -0400

    fix type; format using prettier

--- packages/extension-webgazer/src/index.ts
+++ packages/extension-webgazer/src/index.ts
@@ -3,7 +3,9 @@ import { JsPsych, JsPsychExtension, JsPsychExtensionParameters } from "jspsych";
 // we have to add webgazer to the global window object because webgazer attaches itself to
 // the window when it loads
 declare global {
-  interface Window { webgazer: any; }
+  interface Window {
+    webgazer: any;
+  }
 }
 
 interface InitializeParameters extends JsPsychExtensionParameters {
@@ -21,7 +23,7 @@ interface InitializeParameters extends JsPsychExtensionParameters {
    */
   auto_initialize: boolean;
   /**
-   * The number of milliseconds between each sample. Note that this is only a request, and the 
+   * The number of milliseconds between each sample. Note that this is only a request, and the
    * actual interval will vary depending on processing time.
    * @default 34
    */
@@ -38,8 +40,7 @@ interface OnStartParameters extends JsPsychExtensionParameters {
 }
 
 class WebGazerExtension implements JsPsychExtension {
-
-  constructor(private jsPsych: JsPsych) { }
+  constructor(private jsPsych: JsPsych) {}
 
   // private state for the extension
   // extension authors can define public functions to interact
@@ -47,36 +48,40 @@ class WebGazerExtension implements JsPsychExtension {
   // so that state manipulations are checked.
   private currentTrialData = [];
   private currentTrialTargets = {};
-  private currentTrialSelectors:Array<string>;
-  private domObserver:MutationObserver;
+  private currentTrialSelectors: Array<string>;
+  private domObserver: MutationObserver;
   private webgazer;
-  private initialized:boolean = false;
-  private currentTrialStart:number;
-  private activeTrial:boolean = false;
-  private sampling_interval:number;
-  private round_predictions:boolean;
-  private gazeInterval:number;
-  private gazeUpdateCallbacks:Array<any>;
-  private currentGaze:Object;
+  private initialized = false;
+  private currentTrialStart: number;
+  private activeTrial = false;
+  private sampling_interval: number;
+  private round_predictions: boolean;
+  private gazeInterval: ReturnType<typeof setInterval>;
+  private gazeUpdateCallbacks: Array<any>;
+  private currentGaze: Object;
 
-  // required, will be called at jsPsych.init
-  // should return a Promise
-  initialize({ round_predictions = true, auto_initialize = false, sampling_interval = 34, webgazer }: InitializeParameters): Promise<void> {
-
+  initialize({
+    round_predictions = true,
+    auto_initialize = false,
+    sampling_interval = 34,
+    webgazer,
+  }: InitializeParameters): Promise<void> {
     // set initial state of the extension
     this.round_predictions = round_predictions;
     this.sampling_interval = sampling_interval;
-    this.initialized = false;
-    this.activeTrial = false;
     this.gazeUpdateCallbacks = [];
     this.domObserver = new MutationObserver(this.mutationObserverCallback);
 
     return new Promise((resolve, reject) => {
-      if (typeof webgazer === 'undefined') {
+      if (typeof webgazer === "undefined") {
         if (window.webgazer) {
           this.webgazer = window.webgazer;
         } else {
-          reject(new Error('Webgazer extension failed to initialize. webgazer.js not loaded. Load webgazer.js before calling jsPsych.init()'));
+          reject(
+            new Error(
+              "Webgazer extension failed to initialize. webgazer.js not loaded. Load webgazer.js before calling jsPsych.init()"
+            )
+          );
         }
       } else {
         this.webgazer = webgazer;
@@ -91,7 +96,6 @@ class WebGazerExtension implements JsPsychExtension {
       //state.webgazer.setRegression('threadedRidge');
       //state.webgazer.applyKalmanFilter(false); // kalman filter doesn't seem to work yet with threadedridge.
 
-      
       // hide video by default
       this.hideVideo();
 
@@ -101,19 +105,22 @@ class WebGazerExtension implements JsPsychExtension {
       if (auto_initialize) {
         // starts webgazer, and once it initializes we stop mouseCalibration and
         // pause webgazer data.
-        this.webgazer.begin().then(() => {
-          this.initialized = true;
-          this.stopMouseCalibration();
-          this.pause();
-          resolve();
-        }).catch((error) => {
-          console.error(error);
-          reject(error);
-        });
+        this.webgazer
+          .begin()
+          .then(() => {
+            this.initialized = true;
+            this.stopMouseCalibration();
+            this.pause();
+            resolve();
+          })
+          .catch((error) => {
+            console.error(error);
+            reject(error);
+          });
       } else {
         resolve();
       }
-    })
+    });
   }
 
   // required, will be called when the trial starts (before trial loads)
@@ -122,12 +129,11 @@ class WebGazerExtension implements JsPsychExtension {
     this.currentTrialTargets = {};
     this.currentTrialSelectors = params.targets;
 
-    this.domObserver.observe(this.jsPsych.getDisplayElement(), { childList: true })
+    this.domObserver.observe(this.jsPsych.getDisplayElement(), { childList: true });
   }
 
   // required will be called when the trial loads
   on_load() {
-
     // set current trial start time
     this.currentTrialStart = performance.now();
 
@@ -143,7 +149,6 @@ class WebGazerExtension implements JsPsychExtension {
   // required, will be called when jsPsych.finishTrial() is called
   // must return data object to be merged into data.
   on_finish() {
-
     // pause the eye tracker
     this.stopSampleInterval();
 
@@ -158,27 +163,30 @@ class WebGazerExtension implements JsPsychExtension {
     // send back the gazeData
     return {
       webgazer_data: this.currentTrialData,
-      webgazer_targets: this.currentTrialTargets
-    }
+      webgazer_targets: this.currentTrialTargets,
+    };
   }
 
   start() {
-
     return new Promise<void>(function (resolve, reject) {
-      if (typeof this.webgazer == 'undefined') {
-        const error = 'Failed to start webgazer. Things to check: Is webgazer.js loaded? Is the webgazer extension included in jsPsych.init?'
-        console.error(error)
-        reject(error)
-      }
-      this.webgazer.begin().then(function () {
-        this.initialized = true;
-        this.stopMouseCalibration();
-        this.pause();
-        resolve();
-      }).catch(function (error) {
+      if (typeof this.webgazer == "undefined") {
+        const error =
+          "Failed to start webgazer. Things to check: Is webgazer.js loaded? Is the webgazer extension included in jsPsych.init?";
         console.error(error);
         reject(error);
-      });
+      }
+      this.webgazer
+        .begin()
+        .then(function () {
+          this.initialized = true;
+          this.stopMouseCalibration();
+          this.pause();
+          resolve();
+        })
+        .catch(function (error) {
+          console.error(error);
+          reject(error);
+        });
     });
   }
 
@@ -230,8 +238,8 @@ class WebGazerExtension implements JsPsychExtension {
   pause() {
     this.webgazer.pause();
     // sometimes gaze dot will show and freeze after pause?
-    if (document.querySelector('#webgazerGazeDot')) {
-      document.querySelector<HTMLElement>('#webgazerGazeDot').style.display = 'none';
+    if (document.querySelector("#webgazerGazeDot")) {
+      document.querySelector<HTMLElement>("#webgazerGazeDot").style.display = "none";
     }
   }
 
@@ -240,25 +248,27 @@ class WebGazerExtension implements JsPsychExtension {
   }
 
   stopMouseCalibration() {
-    this.webgazer.removeMouseEventListeners()
+    this.webgazer.removeMouseEventListeners();
   }
 
   startMouseCalibration() {
-    this.webgazer.addMouseEventListeners()
+    this.webgazer.addMouseEventListeners();
   }
 
   calibratePoint(x: number, y: number) {
-    this.webgazer.recordScreenPosition(x, y, 'click');
+    this.webgazer.recordScreenPosition(x, y, "click");
   }
 
   setRegressionType = (regression_type) => {
-    var valid_regression_models = ['ridge', 'weightedRidge', 'threadedRidge'];
+    var valid_regression_models = ["ridge", "weightedRidge", "threadedRidge"];
     if (valid_regression_models.includes(regression_type)) {
-      this.webgazer.setRegression(regression_type)
+      this.webgazer.setRegression(regression_type);
     } else {
-      console.warn('Invalid regression_type parameter for webgazer.setRegressionType. Valid options are ridge, weightedRidge, and threadedRidge.')
+      console.warn(
+        "Invalid regression_type parameter for webgazer.setRegressionType. Valid options are ridge, weightedRidge, and threadedRidge."
+      );
     }
-  }
+  };
 
   getCurrentPrediction() {
     return this.webgazer.getCurrentPrediction();
@@ -270,7 +280,7 @@ class WebGazerExtension implements JsPsychExtension {
       this.gazeUpdateCallbacks = this.gazeUpdateCallbacks.filter((item) => {
         return item !== callback;
       });
-    }
+    };
   }
 
   private handleGazeDataUpdate(gazeData, elapsedTime) {
@@ -278,11 +288,11 @@ class WebGazerExtension implements JsPsychExtension {
       var d = {
         x: this.round_predictions ? Math.round(gazeData.x) : gazeData.x,
         y: this.round_predictions ? Math.round(gazeData.y) : gazeData.y,
-        t: gazeData.t
-      }
+        t: gazeData.t,
+      };
       if (this.activeTrial) {
         //console.log(`handleUpdate: t = ${Math.round(gazeData.t)}, now = ${Math.round(performance.now())}`);
-        d.t = Math.round(gazeData.t - this.currentTrialStart)
+        d.t = Math.round(gazeData.t - this.currentTrialStart);
         this.currentTrialData.push(d); // add data to current trial's data
       }
       this.currentGaze = d;
@@ -298,13 +308,15 @@ class WebGazerExtension implements JsPsychExtension {
     for (const selector of this.currentTrialSelectors) {
       if (!this.currentTrialTargets[selector]) {
         if (this.jsPsych.getDisplayElement().querySelector(selector)) {
-          var coords = this.jsPsych.getDisplayElement().querySelector(selector).getBoundingClientRect();
+          var coords = this.jsPsych
+            .getDisplayElement()
+            .querySelector(selector)
+            .getBoundingClientRect();
           this.currentTrialTargets[selector] = coords;
         }
       }
     }
   }
-
 }
 
 export default WebGazerExtension;
