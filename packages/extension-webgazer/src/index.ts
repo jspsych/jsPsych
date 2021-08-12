af726dbdebf2195090e2883747758bb5659e4508
Josh de Leeuw <josh.deleeuw@gmail.com>
2021-08-12 10:19:50 -0400

    fix arrow function and this scope

--- packages/extension-webgazer/src/index.ts
+++ packages/extension-webgazer/src/index.ts
@@ -65,12 +65,12 @@ class WebGazerExtension implements JsPsychExtension {
   private gazeUpdateCallbacks: Array<any>;
   private currentGaze: Object;
 
-  initialize({
+  initialize = ({
     round_predictions = true,
     auto_initialize = false,
     sampling_interval = 34,
     webgazer,
-  }: InitializeParameters): Promise<void> {
+  }: InitializeParameters): Promise<void> => {
     // set initial state of the extension
     this.round_predictions = round_predictions;
     this.sampling_interval = sampling_interval;
@@ -129,7 +129,7 @@ class WebGazerExtension implements JsPsychExtension {
   }
 
   // required, will be called when the trial starts (before trial loads)
-  on_start(params: OnStartParameters): void {
+  on_start = (params: OnStartParameters): void => {
     this.currentTrialData = [];
     this.currentTrialTargets = {};
     this.currentTrialSelectors = params.targets;
@@ -138,7 +138,7 @@ class WebGazerExtension implements JsPsychExtension {
   }
 
   // required will be called when the trial loads
-  on_load() {
+  on_load = () => {
     // set current trial start time
     this.currentTrialStart = performance.now();
 
@@ -153,7 +153,7 @@ class WebGazerExtension implements JsPsychExtension {
 
   // required, will be called when jsPsych.finishTrial() is called
   // must return data object to be merged into data.
-  on_finish() {
+  on_finish = () => {
     // pause the eye tracker
     this.stopSampleInterval();
 
@@ -172,7 +172,7 @@ class WebGazerExtension implements JsPsychExtension {
     };
   }
 
-  start() {
+  start = () => {
     return new Promise<void>((resolve, reject) => {
       if (typeof this.webgazer == "undefined") {
         const error =
@@ -195,7 +195,7 @@ class WebGazerExtension implements JsPsychExtension {
     });
   }
 
-  startSampleInterval(interval: number = this.sampling_interval) {
+  startSampleInterval = (interval: number = this.sampling_interval) => {
     this.gazeInterval = setInterval(() => {
       this.webgazer.getCurrentPrediction().then(this.handleGazeDataUpdate);
     }, interval);
@@ -204,43 +204,43 @@ class WebGazerExtension implements JsPsychExtension {
     this.webgazer.getCurrentPrediction().then(this.handleGazeDataUpdate);
   }
 
-  stopSampleInterval() {
+  stopSampleInterval = () => {
     clearInterval(this.gazeInterval);
   }
 
-  isInitialized() {
+  isInitialized = () => {
     return this.initialized;
   }
 
-  faceDetected() {
+  faceDetected = () => {
     return this.webgazer.getTracker().predictionReady;
   }
 
-  showPredictions() {
+  showPredictions = () => {
     this.webgazer.showPredictionPoints(true);
   }
 
-  hidePredictions() {
+  hidePredictions = () => {
     this.webgazer.showPredictionPoints(false);
   }
 
-  showVideo() {
+  showVideo = () => {
     this.webgazer.showVideo(true);
     this.webgazer.showFaceOverlay(true);
     this.webgazer.showFaceFeedbackBox(true);
   }
 
-  hideVideo() {
+  hideVideo = () => {
     this.webgazer.showVideo(false);
     this.webgazer.showFaceOverlay(false);
     this.webgazer.showFaceFeedbackBox(false);
   }
 
-  resume() {
+  resume = () => {
     this.webgazer.resume();
   }
 
-  pause() {
+  pause = () => {
     this.webgazer.pause();
     // sometimes gaze dot will show and freeze after pause?
     if (document.querySelector("#webgazerGazeDot")) {
@@ -248,19 +248,19 @@ class WebGazerExtension implements JsPsychExtension {
     }
   }
 
-  resetCalibration() {
+  resetCalibration = () => {
     this.webgazer.clearData();
   }
 
-  stopMouseCalibration() {
+  stopMouseCalibration = () => {
     this.webgazer.removeMouseEventListeners();
   }
 
-  startMouseCalibration() {
+  startMouseCalibration = () => {
     this.webgazer.addMouseEventListeners();
   }
 
-  calibratePoint(x: number, y: number) {
+  calibratePoint = (x: number, y: number) => {
     this.webgazer.recordScreenPosition(x, y, "click");
   }
 
@@ -275,11 +275,11 @@ class WebGazerExtension implements JsPsychExtension {
     }
   };
 
-  getCurrentPrediction() {
+  getCurrentPrediction = () => {
     return this.webgazer.getCurrentPrediction();
   }
 
-  onGazeUpdate(callback) {
+  onGazeUpdate = (callback) => {
     this.gazeUpdateCallbacks.push(callback);
     return () => {
       this.gazeUpdateCallbacks = this.gazeUpdateCallbacks.filter((item) => {
@@ -288,7 +288,7 @@ class WebGazerExtension implements JsPsychExtension {
     };
   }
 
-  private handleGazeDataUpdate(gazeData, elapsedTime) {
+  private handleGazeDataUpdate = (gazeData, elapsedTime) => {
     if (gazeData !== null) {
       var d = {
         x: this.round_predictions ? Math.round(gazeData.x) : gazeData.x,
@@ -309,7 +309,7 @@ class WebGazerExtension implements JsPsychExtension {
     }
   }
 
-  private mutationObserverCallback(mutationsList, observer) {
+  private mutationObserverCallback = (mutationsList, observer) => {
     for (const selector of this.currentTrialSelectors) {
       if (!this.currentTrialTargets[selector]) {
         if (this.jsPsych.getDisplayElement().querySelector(selector)) {
