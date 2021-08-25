6006fec96a83a12b536d6998bdd1204ea8054338
bjoluc <mail@bjoluc.de>
2021-08-25 20:51:09 +0200

    Prepare @jspsych/config for external use

--- packages/plugin-survey-text/rollup.config.mjs
+++ packages/plugin-survey-text/rollup.config.mjs
@@ -1,3 +1,3 @@
-import { makeRollupConfigForPlugin } from "@jspsych/config/rollup.mjs";
+import { makeRollupConfig } from "@jspsych/config/rollup";
 
-export default makeRollupConfigForPlugin("jsPsychSurveyText");
+export default makeRollupConfig("jsPsychSurveyText");
