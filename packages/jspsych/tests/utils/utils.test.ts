b5f06d41441ea236de367093229c65ca88ead120
Josh de Leeuw <josh.deleeuw@gmail.com>
2021-08-25 10:56:40 -0400

    update tests for es6

--- packages/jspsych/tests/utils/utils.test.ts
+++ packages/jspsych/tests/utils/utils.test.ts
@@ -2,48 +2,45 @@ import { deepCopy, unique } from "../../src/modules/utils";
 
 describe("unique", () => {
   test("generates unique array when there are duplicates", () => {
-    var arr = [1, 1, 2, 2, 3, 3];
-    var out = unique(arr);
+    const arr = [1, 1, 2, 2, 3, 3];
+    const out = unique(arr);
     expect(out).toEqual([1, 2, 3]);
     expect(out).not.toEqual(arr);
   });
 
   test("generates same array when there are no duplicates", () => {
-    var arr = [1, 2, 3];
-    var out = unique(arr);
+    const arr = [1, 2, 3];
+    const out = unique(arr);
     expect(out).toEqual(arr);
   });
 });
 
 describe("deepCopy", () => {
   test("works for objects", () => {
-    var o = { a: 1, b: { c: 2, d: 3 } };
-    var o2 = deepCopy(o);
+    const o = { a: 1, b: { c: 2, d: 3 } };
+    const o2 = deepCopy(o);
     o2.b.c = 4;
     expect(o.b.c).toBe(2);
   });
   test("works for objects with arrays", () => {
-    var o = { a: 1, b: [2, 3] };
-    var o2 = deepCopy(o);
+    const o = { a: 1, b: [2, 3] };
+    const o2 = deepCopy(o);
     o2.b[0] = 4;
     expect(JSON.stringify(o2.b)).toBe(JSON.stringify([4, 3]));
     expect(o.b[0]).toBe(2);
   });
   test("works for objects with functions", () => {
-    var c = 0;
-    var o = {
+    const o = {
       a: 1,
       b: () => {
-        c = 2;
+        return 2;
       },
     };
-    var o2 = deepCopy(o);
+    const o2 = deepCopy(o);
     o2.b = () => {
-      c = 1;
+      return 1;
     };
-    o.b();
-    expect(c).toBe(2);
-    o2.b();
-    expect(c).toBe(1);
+    expect(o.b()).toBe(2);
+    expect(o2.b()).toBe(1);
   });
 });
