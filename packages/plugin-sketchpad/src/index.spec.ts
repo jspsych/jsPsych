ed9833a045a573c7ff293a08c70684f3ed235753
Josh de Leeuw <josh.deleeuw@gmail.com>
2021-10-26 12:22:24 -0400

    continue dev work on sketchpad

--- packages/plugin-sketchpad/src/index.spec.ts
+++ packages/plugin-sketchpad/src/index.spec.ts
@@ -5,6 +5,20 @@ import sketchpad from ".";
 jest.useFakeTimers();
 
 describe("sketchpad", () => {
+  test("basic load with defaults", async () => {
+    const { displayElement, getHTML, expectFinished } = await startTimeline([
+      {
+        type: sketchpad,
+      },
+    ]);
+
+    const canvas = displayElement.querySelector("canvas");
+    expect(canvas).not.toBeNull();
+
+    clickTarget(displayElement.querySelector("#sketchpad-end"));
+    await expectFinished();
+  });
+
   test("displays canvas with different dimensions", async () => {
     const { displayElement, getHTML, expectFinished } = await startTimeline([
       {
@@ -14,11 +28,100 @@ describe("sketchpad", () => {
       },
     ]);
 
-    console.log(getHTML());
+    const canvas = displayElement.querySelector("canvas");
+    expect(canvas.getAttribute("width")).toBe("800");
+    expect(canvas.getAttribute("height")).toBe("300");
 
-    const canvas_rect = displayElement.querySelector("canvas");
-    expect(canvas_rect.getAttribute("width")).toBe("800");
-    expect(canvas_rect.getAttribute("height")).toBe("300");
+    clickTarget(displayElement.querySelector("#sketchpad-end"));
+    await expectFinished();
+  });
+
+  test("renders a circular canvas", async () => {
+    const { displayElement, getHTML, expectFinished } = await startTimeline([
+      {
+        type: sketchpad,
+        canvas_diameter: 300,
+        canvas_shape: "circle",
+      },
+    ]);
+
+    const canvas: HTMLElement = displayElement.querySelector("canvas");
+    expect(canvas.className).toContain("sketchpad-circle");
+    expect(canvas.getAttribute("width")).toBe("300");
+    expect(canvas.getAttribute("height")).toBe("300");
+
+    clickTarget(displayElement.querySelector("#sketchpad-end"));
+    await expectFinished();
+  });
+
+  test("prompt shows abovecanvas", async () => {
+    const { displayElement, getHTML, expectFinished } = await startTimeline([
+      {
+        type: sketchpad,
+        prompt: '<p id="prompt">Foo</p>',
+        prompt_location: "abovecanvas",
+      },
+    ]);
+
+    const display_content = Array.from(displayElement.children, (x) => {
+      return (x as HTMLElement).id;
+    });
+
+    expect(display_content.indexOf("prompt")).toBeLessThan(
+      display_content.indexOf("sketchpad-canvas")
+    );
+
+    clickTarget(displayElement.querySelector("#sketchpad-end"));
+    await expectFinished();
+  });
+
+  test("prompt shows belowcanvas", async () => {
+    const { displayElement, getHTML, expectFinished } = await startTimeline([
+      {
+        type: sketchpad,
+        prompt: '<p id="prompt">Foo</p>',
+        prompt_location: "belowcanvas",
+      },
+    ]);
+
+    const display_content = Array.from(displayElement.children, (x) => {
+      return (x as HTMLElement).id;
+    });
+
+    expect(display_content.indexOf("prompt")).toBeGreaterThan(
+      display_content.indexOf("sketchpad-canvas")
+    );
+    expect(display_content.indexOf("prompt")).toBeGreaterThan(
+      display_content.indexOf("sketchpad-controls")
+    );
+    expect(display_content.indexOf("prompt")).toBeLessThan(display_content.indexOf("finish-btn"));
+
+    clickTarget(displayElement.querySelector("#sketchpad-end"));
+    await expectFinished();
+  });
+
+  test("prompt shows belowbutton", async () => {
+    const { displayElement, getHTML, expectFinished } = await startTimeline([
+      {
+        type: sketchpad,
+        prompt: '<p id="prompt">Foo</p>',
+        prompt_location: "belowbutton",
+      },
+    ]);
+
+    const display_content = Array.from(displayElement.children, (x) => {
+      return (x as HTMLElement).id;
+    });
+
+    expect(display_content.indexOf("prompt")).toBeGreaterThan(
+      display_content.indexOf("sketchpad-canvas")
+    );
+    expect(display_content.indexOf("prompt")).toBeGreaterThan(
+      display_content.indexOf("sketchpad-controls")
+    );
+    expect(display_content.indexOf("prompt")).toBeGreaterThan(
+      display_content.indexOf("finish-btn")
+    );
 
     clickTarget(displayElement.querySelector("#sketchpad-end"));
     await expectFinished();
--- packages/plugin-sketchpad/src/index.ts
+++ packages/plugin-sketchpad/src/index.ts
@@ -53,6 +53,13 @@ const info = <const>{
       default: null,
     },
     /**
+     * Background color of the canvas.
+     */
+    background_color: {
+      type: ParameterType.STRING,
+      default: "#ffffff",
+    },
+    /**
      * The width of the strokes on the canvas.
      */
     stroke_width: {
@@ -110,6 +117,91 @@ const info = <const>{
       type: ParameterType.KEY,
       default: null,
     },
+    /**
+     * Whether to show the button that ends the trial
+     */
+    show_finished_button: {
+      type: ParameterType.BOOL,
+      default: true,
+    },
+    /**
+     * The label for the button that ends the trial
+     */
+    finished_button_label: {
+      type: ParameterType.STRING,
+      default: "Finished",
+    },
+    /**
+     * Whether to show the button that clears the entire drawing.
+     */
+    show_clear_button: {
+      type: ParameterType.BOOL,
+      default: true,
+    },
+    /**
+     * The label for the button that clears the entire drawing.
+     */
+    clear_button_label: {
+      type: ParameterType.STRING,
+      default: "Clear",
+    },
+    /**
+     * Whether to show the button that enables an undo action.
+     */
+    show_undo_button: {
+      type: ParameterType.BOOL,
+      default: true,
+    },
+    /**
+     * The label for the button that performs an undo action.
+     */
+    undo_button_label: {
+      type: ParameterType.STRING,
+      default: "Undo",
+    },
+    /**
+     * Whether to show the button that enables an redo action. `show_undo_button` must also
+     * be `true` for the redo button to show.
+     */
+    show_redo_button: {
+      type: ParameterType.BOOL,
+      default: true,
+    },
+    /**
+     * The label for the button that performs an redo action.
+     */
+    redo_button_label: {
+      type: ParameterType.STRING,
+      default: "Redo",
+    },
+    /**
+     * Array of keys that will end the trial when pressed.
+     */
+    choices: {
+      type: ParameterType.KEYS,
+      default: "NO_KEYS",
+    },
+    /**
+     * Length of time before trial ends. If `null` the trial will not timeout.
+     */
+    trial_duration: {
+      type: ParameterType.INT,
+      default: null,
+    },
+    /**
+     * Whether to show a countdown timer for the remaining trial duration
+     */
+    show_countdown_trial_duration: {
+      type: ParameterType.BOOL,
+      default: false,
+    },
+    /**
+     * The html for the countdown timer.
+     */
+    countdown_timer_html: {
+      type: ParameterType.HTML_STRING,
+      default: `<span id="sketchpad-timer"></span> remaining`,
+    },
   },
 };
 
@@ -139,61 +231,35 @@ class SketchpadPlugin implements JsPsychPlugin<Info> {
   private start_time;
   private mouse_position = { x: 0, y: 0 };
   private draw_key_held = false;
+  private timer_interval;
 
   constructor(private jsPsych: JsPsych) {}
 
   trial(display_element: HTMLElement, trial: TrialType<Info>, on_load: () => void) {
     this.display = display_element;
     this.params = trial;
-
-    document.querySelector("head").insertAdjacentHTML(
-      "beforeend",
-      `<style id="sketchpad-styles">
-        #sketchpad-controls {
-          line-height: 1; 
-          width:${
-            this.params.canvas_shape == "rectangle"
-              ? this.params.canvas_width
-              : this.params.canvas_diameter
-          }px; 
-          display: flex; 
-          justify-content: space-between; 
-          flex-wrap: wrap;
-        }
-        #sketchpad-color-palette { 
-          display: inline-block; text-align:left; flex-basis: 50%;
-        }
-        .sketchpad-color-select { 
-          cursor: pointer; height: 33px; width: 33px; border-radius: 4px; padding: 0; border: 1px solid #ccc; 
-        }
-        #sketchpad-canvas {
-          touch-action: none;
-          border: ${this.params.canvas_border_width}px solid ${this.params.canvas_border_color};
-        }
-        .sketchpad-circle {
-          border-radius: ${this.params.canvas_diameter / 2}px;
-        }
-      </style>`
-    );
-
     this.current_stroke_color = trial.stroke_color;
 
-    this.render_display();
+    this.init_display();
 
     this.setup_event_listeners();
 
+    this.add_background_color();
     this.add_background_image().then(() => {
       on_load();
     });
 
     this.start_time = performance.now();
+    this.set_trial_duration_timer();
 
     return new Promise((resolve, reject) => {
       this.trial_finished_handler = resolve;
     });
   }
 
-  private render_display() {
+  private init_display() {
+    this.add_css();
+
     let canvas_html;
     if (this.params.canvas_shape == "rectangle") {
       canvas_html = `
@@ -224,27 +290,43 @@ class SketchpadPlugin implements JsPsychPlugin<Info> {
     }
     sketchpad_controls += `</div>`;
 
-    sketchpad_controls += `<div id="sketchpad-actions" style="display:inline-block; text-align:right;">
-        <button class="jspsych-btn" id="sketchpad-clear">Clear</button>
-        <button class="jspsych-btn" id="sketchpad-undo" disabled>Undo</button>
-        <button class="jspsych-btn" id="sketchpad-redo" disabled>Redo</button>
-      </div>`;
+    sketchpad_controls += `<div id="sketchpad-actions">`;
+    if (this.params.show_clear_button) {
+      sketchpad_controls += `<button class="jspsych-btn" id="sketchpad-clear" disabled>${this.params.clear_button_label}</button>`;
+    }
+    if (this.params.show_undo_button) {
+      sketchpad_controls += `<button class="jspsych-btn" id="sketchpad-undo" disabled>${this.params.undo_button_label}</button>`;
+      if (this.params.show_redo_button) {
+        sketchpad_controls += `<button class="jspsych-btn" id="sketchpad-redo" disabled>${this.params.redo_button_label}</button>`;
+      }
+    }
+    sketchpad_controls += `</div></div>`;
 
     canvas_html += sketchpad_controls;
 
-    const finish_button_html = `<p style="flex-basis:100%;"><button class="jspsych-btn" id="sketchpad-end">Finished</button></p>`;
+    let finish_button_html = "";
+    if (this.params.show_finished_button) {
+      finish_button_html = `<p id="finish-btn"><button class="jspsych-btn" id="sketchpad-end">Finished</button></p>`;
+    }
+
+    let timer_html = "";
+    if (this.params.show_countdown_trial_duration && this.params.trial_duration) {
+      timer_html = `<p id="countdown-timer">${this.params.countdown_timer_html}</p>`;
+    }
 
     let display_html;
     if (this.params.prompt !== null) {
       if (this.params.prompt_location == "abovecanvas") {
-        display_html = this.params.prompt + canvas_html + finish_button_html;
+        display_html = this.params.prompt + timer_html + canvas_html + finish_button_html;
       }
       if (this.params.prompt_location == "belowcanvas") {
-        display_html = canvas_html + this.params.prompt + finish_button_html;
+        display_html = timer_html + canvas_html + this.params.prompt + finish_button_html;
       }
       if (this.params.prompt_location == "belowbutton") {
-        display_html = canvas_html + finish_button_html + this.params.prompt;
+        display_html = timer_html + canvas_html + finish_button_html + this.params.prompt;
       }
+    } else {
+      display_html = timer_html + canvas_html + finish_button_html;
     }
 
     this.display.innerHTML = display_html;
@@ -258,7 +340,11 @@ class SketchpadPlugin implements JsPsychPlugin<Info> {
       this.mouse_position = { x: e.clientX, y: e.clientY };
     });
 
-    this.display.querySelector("#sketchpad-end").addEventListener("click", this.end_trial);
+    if (this.params.show_finished_button) {
+      this.display.querySelector("#sketchpad-end").addEventListener("click", () => {
+        this.end_trial("button");
+      });
+    }
 
     this.sketchpad.addEventListener("pointerdown", this.start_draw);
     this.sketchpad.addEventListener("pointermove", this.move_draw);
@@ -302,9 +388,15 @@ class SketchpadPlugin implements JsPsychPlugin<Info> {
       });
     }
 
-    this.display.querySelector("#sketchpad-undo").addEventListener("click", this.undo);
-    this.display.querySelector("#sketchpad-redo").addEventListener("click", this.redo);
-    this.display.querySelector("#sketchpad-clear").addEventListener("click", this.clear);
+    if (this.params.show_undo_button) {
+      this.display.querySelector("#sketchpad-undo").addEventListener("click", this.undo);
+      if (this.params.show_redo_button) {
+        this.display.querySelector("#sketchpad-redo").addEventListener("click", this.redo);
+      }
+    }
+    if (this.params.show_clear_button) {
+      this.display.querySelector("#sketchpad-clear").addEventListener("click", this.clear);
+    }
 
     const color_btns = Array.from(this.display.querySelectorAll(".sketchpad-color-select"));
     for (const btn of color_btns) {
@@ -312,6 +404,71 @@ class SketchpadPlugin implements JsPsychPlugin<Info> {
         const target = e.target as HTMLButtonElement;
         this.current_stroke_color = target.getAttribute("data-color");
       });
+    }
+
+    this.jsPsych.pluginAPI.getKeyboardResponse({
+      callback_function: this.after_key_response,
+      valid_responses: this.params.choices,
+      persist: false,
+      allow_held_key: false,
+    });
+  }
+
+  private add_css() {
+    document.querySelector("head").insertAdjacentHTML(
+      "beforeend",
+      `<style id="sketchpad-styles">
+        #sketchpad-controls {
+          line-height: 1; 
+          width:${
+            this.params.canvas_shape == "rectangle"
+              ? this.params.canvas_width + this.params.canvas_border_width * 2
+              : this.params.canvas_diameter + this.params.canvas_border_width * 2
+          }px; 
+          display: flex; 
+          justify-content: space-between; 
+          flex-wrap: wrap;
+        }
+        #sketchpad-color-palette { 
+          display: inline-block; text-align:left; flex-basis: 50%;
+        }
+        .sketchpad-color-select { 
+          cursor: pointer; height: 33px; width: 33px; border-radius: 4px; padding: 0; border: 1px solid #ccc; 
+        }
+        #sketchpad-actions {
+          display:inline-block; text-align:right;
+        }
+        #sketchpad-actions button {
+          margin-left: 4px;
+        }
+        #sketchpad-canvas {
+          touch-action: none;
+          border: ${this.params.canvas_border_width}px solid ${this.params.canvas_border_color};
+        }
+        .sketchpad-circle {
+          border-radius: ${this.params.canvas_diameter / 2}px;
+        }
+        #countdown-timer {
+          width:${
+            this.params.canvas_shape == "rectangle"
+              ? this.params.canvas_width + this.params.canvas_border_width * 2
+              : this.params.canvas_diameter + this.params.canvas_border_width * 2
+          }px; 
+          text-align: right;
+          font-size: 12px; 
+          margin-bottom: 0.2em;
+        }
+      </style>`
+    );
+  }
+
+  private add_background_color() {
+    this.ctx.fillStyle = this.params.background_color;
+    if (this.params.canvas_shape == "rectangle") {
+      this.ctx.fillRect(0, 0, this.params.canvas_width, this.params.canvas_height);
+    }
+    if (this.params.canvas_shape == "circle") {
+      this.ctx.fillRect(0, 0, this.params.canvas_diameter, this.params.canvas_diameter);
     }
   }
 
@@ -337,7 +494,7 @@ class SketchpadPlugin implements JsPsychPlugin<Info> {
     const y = e.clientY - this.sketchpad.getBoundingClientRect().top;
 
     this.undo_history = [];
-    (this.display.querySelector("#sketchpad-redo") as HTMLButtonElement).disabled = true;
+    this.set_redo_btn_state(false);
 
     this.ctx.beginPath();
     this.ctx.moveTo(x, y);
@@ -378,13 +535,15 @@ class SketchpadPlugin implements JsPsychPlugin<Info> {
         t: Math.round(performance.now() - this.start_time),
       });
       this.strokes.push(this.stroke);
-      (this.display.querySelector("#sketchpad-undo") as HTMLButtonElement).disabled = false;
+      this.set_undo_btn_state(true);
+      this.set_clear_btn_state(true);
     }
     this.is_drawing = false;
   }
 
   private render_drawing() {
     this.ctx.clearRect(0, 0, this.sketchpad.width, this.sketchpad.height);
+    this.add_background_color();
     this.ctx.drawImage(this.background_image, 0, 0);
     for (const stroke of this.strokes) {
       for (const m of stroke) {
@@ -405,18 +564,18 @@ class SketchpadPlugin implements JsPsychPlugin<Info> {
 
   private undo() {
     this.undo_history.push(this.strokes.pop());
-    (this.display.querySelector("#sketchpad-redo") as HTMLButtonElement).disabled = false;
+    this.set_redo_btn_state(true);
     if (this.strokes.length == 0) {
-      (this.display.querySelector("#sketchpad-undo") as HTMLButtonElement).disabled = true;
+      this.set_undo_btn_state(false);
     }
     this.render_drawing();
   }
 
   private redo() {
     this.strokes.push(this.undo_history.pop());
-    (this.display.querySelector("#sketchpad-undo") as HTMLButtonElement).disabled = false;
+    this.set_undo_btn_state(true);
     if (this.undo_history.length == 0) {
-      (this.display.querySelector("#sketchpad-redo") as HTMLButtonElement).disabled = true;
+      this.set_redo_btn_state(false);
     }
     this.render_drawing();
   }
@@ -425,14 +584,73 @@ class SketchpadPlugin implements JsPsychPlugin<Info> {
     this.strokes = [];
     this.undo_history = [];
     this.render_drawing();
-    (this.display.querySelector("#sketchpad-redo") as HTMLButtonElement).disabled = true;
-    (this.display.querySelector("#sketchpad-undo") as HTMLButtonElement).disabled = true;
+    this.set_redo_btn_state(false);
+    this.set_undo_btn_state(false);
+    this.set_clear_btn_state(false);
   }
 
-  private end_trial() {
+  private set_undo_btn_state(enabled: boolean) {
+    if (this.params.show_undo_button) {
+      (this.display.querySelector("#sketchpad-undo") as HTMLButtonElement).disabled = !enabled;
+    }
+  }
+
+  private set_redo_btn_state(enabled: boolean) {
+    if (this.params.show_redo_button) {
+      (this.display.querySelector("#sketchpad-redo") as HTMLButtonElement).disabled = !enabled;
+    }
+  }
+
+  private set_clear_btn_state(enabled: boolean) {
+    if (this.params.show_redo_button) {
+      (this.display.querySelector("#sketchpad-clear") as HTMLButtonElement).disabled = !enabled;
+    }
+  }
+
+  private set_trial_duration_timer() {
+    if (this.params.trial_duration !== null) {
+      this.jsPsych.pluginAPI.setTimeout(() => {
+        this.end_trial();
+      }, this.params.trial_duration);
+      if (this.params.show_countdown_trial_duration) {
+        this.timer_interval = setInterval(() => {
+          const remaining = this.params.trial_duration - (performance.now() - this.start_time);
+          let minutes = Math.floor(remaining / 1000 / 60);
+          let seconds = Math.ceil((remaining - minutes * 1000 * 60) / 1000);
+          if (seconds == 60) {
+            seconds = 0;
+            minutes++;
+          }
+          const minutes_str = minutes.toString();
+          const seconds_str = seconds.toString().padStart(2, "0");
+          const timer_span = this.display.querySelector("#sketchpad-timer");
+          if (timer_span) {
+            timer_span.innerHTML = `${minutes_str}:${seconds_str}`;
+          }
+          if (remaining <= 0) {
+            if (timer_span) {
+              timer_span.innerHTML = `0:00`;
+            }
+            clearInterval(this.timer_interval);
+          }
+        }, 250);
+      }
+    }
+  }
+
+  private after_key_response(info) {
+    this.end_trial(info.key);
+  }
+
+  private end_trial(response = null) {
+    this.jsPsych.pluginAPI.clearAllTimeouts();
+    this.jsPsych.pluginAPI.cancelAllKeyboardResponses();
+    clearInterval(this.timer_interval);
+
     const trial_data = <any>{};
 
     trial_data.rt = performance.now() - this.start_time;
+    trial_data.response = response;
 
     if (this.params.save_final_image) {
       trial_data.png = this.sketchpad.toDataURL();
