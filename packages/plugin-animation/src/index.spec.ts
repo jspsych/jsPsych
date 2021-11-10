f7bed4d4d7505261ee61a37e10a95841cc4308b0
Josh de Leeuw <josh.deleeuw@gmail.com>
2021-11-10 08:00:28 -0500

    Added simulation mode to animation plugin; fixed bug in `frame_isi`

--- packages/plugin-animation/src/index.spec.ts
+++ packages/plugin-animation/src/index.spec.ts
@@ -1,4 +1,4 @@
-import { startTimeline } from "@jspsych/test-utils";
+import { pressKey, simulateTimeline, startTimeline } from "@jspsych/test-utils";
 
 import animation from ".";
 
@@ -20,5 +20,60 @@ describe("animation plugin", () => {
     jest.advanceTimersByTime(250);
 
     await expectFinished();
+  });
+});
+
+describe("animation simulation", () => {
+  test("data mode works", async () => {
+    const timeline = [
+      {
+        type: animation,
+        stimuli: ["1.png", "2.png", "3.png", "4.png"],
+        sequence_reps: 3,
+        render_on_canvas: false,
+      },
+    ];
+
+    const { expectFinished, getData } = await simulateTimeline(timeline);
+
+    await expectFinished();
+
+    const data = getData().values()[0];
+
+    expect(data.animation_sequence.length).toBe(12);
+    expect(data.response).not.toBeUndefined();
+  });
+
+  test("visual mode works", async () => {
+    const timeline = [
+      {
+        type: animation,
+        stimuli: ["1.png", "2.png", "3.png", "4.png"],
+        sequence_reps: 3,
+        frame_time: 50,
+        frame_isi: 50,
+        render_on_canvas: false,
+      },
+    ];
+
+    const { expectFinished, expectRunning, getHTML, getData, displayElement } =
+      await simulateTimeline(timeline, "visual");
+
+    await expectRunning();
+
+    expect(getHTML()).toContain("1.png");
+    jest.advanceTimersByTime(50);
+    expect(displayElement.querySelector("img").style.visibility).toBe("hidden");
+    jest.advanceTimersByTime(50);
+    expect(getHTML()).toContain("2.png");
+
+    jest.runAllTimers();
+
+    await expectFinished();
+
+    const data = getData().values()[0];
+
+    expect(data.animation_sequence.length).toBe(24);
+    expect(data.response).not.toBeUndefined();
   });
 });
