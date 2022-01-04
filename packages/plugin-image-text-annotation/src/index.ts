0776ec485f8aac48965965c71e4bfee42485fdc3
Josh de Leeuw <josh.deleeuw@gmail.com>
2022-01-04 10:22:44 -0500

    improve drag/move constraints

--- packages/plugin-image-text-annotation/src/index.ts
+++ packages/plugin-image-text-annotation/src/index.ts
@@ -525,15 +525,15 @@ class AnnotationBox {
   }
 
   moveHandler(e) {
-    const container_box = this.container.getBoundingClientRect();
+    const container = this.container.getBoundingClientRect();
 
-    let x = Math.round(e.clientX - container_box.left);
-    let y = Math.round(e.clientY - container_box.top);
+    let x = Math.round(e.clientX - container.left);
+    let y = Math.round(e.clientY - container.top);
 
-    x = Math.min(x, container_box.width);
+    x = Math.min(x, container.width);
     x = Math.max(x, 0);
 
-    y = Math.min(y, container_box.height);
+    y = Math.min(y, container.height);
     y = Math.max(y, 0);
 
     this.setEndCoords(x, y);
@@ -544,10 +544,8 @@ class AnnotationBox {
   }
 
   startDrag(e) {
-    this.drag_offset_x =
-      Math.round(e.clientX - this.container.getBoundingClientRect().left) - this.start_x;
-    this.drag_offset_y =
-      Math.round(e.clientY - this.container.getBoundingClientRect().top) - this.start_y;
+    this.drag_offset_x = Math.round(e.clientX - this.element.getBoundingClientRect().left);
+    this.drag_offset_y = Math.round(e.clientY - this.element.getBoundingClientRect().top);
 
     this.container.addEventListener("mousemove", this.dragHandler);
     this.container.addEventListener("mouseup", this.stopDrag);
@@ -559,26 +557,27 @@ class AnnotationBox {
   }
 
   dragHandler(e) {
-    const container_box = this.container.getBoundingClientRect();
+    const box = this.element.getBoundingClientRect();
+    const container = this.container.getBoundingClientRect();
 
-    const x = Math.round(e.clientX - container_box.left);
-    const y = Math.round(e.clientY - container_box.top);
+    const box_rel = {
+      left: box.left - container.left,
+      top: box.top - container.top,
+    };
 
-    const dx = x - this.drag_offset_x - this.start_x;
-    const dy = y - this.drag_offset_y - this.start_y;
+    const x = Math.round(e.clientX - container.left);
+    const y = Math.round(e.clientY - container.top);
 
-    if (
-      this.start_x + dx < container_box.width &&
-      this.end_x + dx < container_box.width &&
-      this.start_x + dx > 0 &&
-      this.end_x + dx > 0 &&
-      this.start_y + dy < container_box.height &&
-      this.end_y + dy < container_box.height &&
-      this.start_y + dy > 0 &&
-      this.start_y + dy > 0
-    ) {
-      this.translate(dx, dy);
-    }
+    let dx = x - this.drag_offset_x - box_rel.left;
+    let dy = y - this.drag_offset_y - box_rel.top;
+
+    dx = Math.min(dx, container.width - box_rel.left - box.width);
+    dx = Math.max(dx, -box_rel.left);
+
+    dy = Math.min(dy, container.height - box_rel.top - box.height);
+    dy = Math.max(dy, -box_rel.top);
+
+    this.translate(dx, dy);
   }
 
   area() {
