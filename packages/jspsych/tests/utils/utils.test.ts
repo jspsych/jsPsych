236cac8a21bbc7091226e23cc75ca962145d8c2b
Josh de Leeuw <josh.deleeuw@gmail.com>
2023-05-16 21:35:20 -0400

    add deepMerge utility function

--- packages/jspsych/tests/utils/utils.test.ts
+++ packages/jspsych/tests/utils/utils.test.ts
@@ -1,4 +1,4 @@
-import { deepCopy, unique } from "../../src/modules/utils";
+import { deepCopy, deepMerge, unique } from "../../src/modules/utils";
 
 describe("unique", () => {
   test("generates unique array when there are duplicates", () => {
@@ -42,5 +42,55 @@ describe("deepCopy", () => {
     };
     expect(o.b()).toBe(2);
     expect(o2.b()).toBe(1);
+  });
+});
+
+describe("deepMerge", () => {
+  it("should merge two objects with nested properties", () => {
+    const obj1 = { a: 1, b: { c: { d: 1 } } };
+    const obj2 = { b: { c: { e: 2 } }, f: 3 };
+    const expected = { a: 1, b: { c: { d: 1, e: 2 } }, f: 3 };
+    const result = deepMerge(obj1, obj2);
+    expect(result).toEqual(expected);
+  });
+
+  it("should overwrite properties in obj1 with properties in obj2", () => {
+    const obj1 = { a: 1, b: { c: { d: 1 } } };
+    const obj2 = { a: 2, b: { c: { d: 2 } } };
+    const expected = { a: 2, b: { c: { d: 2 } } };
+    const result = deepMerge(obj1, obj2);
+    expect(result).toEqual(expected);
+  });
+
+  it("should handle null and undefined values", () => {
+    const obj1 = { a: null, b: { c: undefined } };
+    const obj2 = { a: 1, b: { c: { d: 1 } } };
+    const expected = { a: 1, b: { c: { d: 1 } } };
+    const result = deepMerge(obj1, obj2);
+    expect(result).toEqual(expected);
+  });
+
+  it("should handle empty objects", () => {
+    const obj1 = { a: 1, b: {} };
+    const obj2 = { b: { c: 2 } };
+    const expected = { a: 1, b: { c: 2 } };
+    const result = deepMerge(obj1, obj2);
+    expect(result).toEqual(expected);
+  });
+
+  it("should handle when one property is an object and the corresponding property is not", () => {
+    const obj1 = { a: 1, b: { c: { d: 1 } } };
+    const obj2 = { a: 2, b: 3 };
+    const expected = { a: 2, b: 3 };
+    const result = deepMerge(obj1, obj2);
+    expect(result).toEqual(expected);
+  });
+
+  it("should handle when one property is an object and the corresponding property is not, reversed", () => {
+    const obj1 = { a: 1, b: { c: { d: 1 } } };
+    const obj2 = { a: 2, b: 3 };
+    const expected = { a: 1, b: { c: { d: 1 } } };
+    const result = deepMerge(obj2, obj1);
+    expect(result).toEqual(expected);
   });
 });
