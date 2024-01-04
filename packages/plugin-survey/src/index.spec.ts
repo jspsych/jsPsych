ab6cb2aa66d0a46cc08bcdb430e5433b5d7fe7d6
Becky Gilbert <beckyannegilbert@gmail.com>
2024-01-04 15:26:38 -0800

    add test for combining survey_json and survey_function parameters

--- packages/plugin-survey/src/index.spec.ts
+++ packages/plugin-survey/src/index.spec.ts
@@ -62,4 +62,49 @@ describe("survey plugin", () => {
     clickTarget(complete_button);
     await expectFinished();
   });
+
+  test("survey_json can be combined with survey_function", async () => {
+    const survey_json = {
+      pages: [
+        {
+          name: "test_page",
+          elements: [
+            {
+              type: "radiogroup",
+              name: "question_1",
+              choices: [
+                { value: 1, text: "Option 1" },
+                { value: 2, text: "Option 2" },
+              ],
+            },
+          ],
+        },
+      ],
+    };
+
+    const survey_function = (survey) => {
+      const page = survey.getPageByName("test_page");
+      page.addNewQuestion("comment", "question_2");
+    };
+
+    const { displayElement, expectRunning, expectFinished, getData } = await startTimeline([
+      {
+        type: survey,
+        survey_json: JSON.stringify(survey_json),
+        survey_function: survey_function,
+      },
+    ]);
+
+    await expectRunning();
+
+    expect(displayElement.querySelector('div[data-name="question_1"]')).not.toBeNull();
+    expect(displayElement.querySelector('div[data-name="question_2"]')).not.toBeNull();
+
+    const complete_button = displayElement.querySelector(
+      'input[type="button"].sd-navigation__complete-btn'
+    );
+    expect(complete_button).not.toBeNull();
+    clickTarget(complete_button);
+    await expectFinished();
+  });
 });
