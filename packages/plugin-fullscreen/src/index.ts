fbe642c50fb7369f85869f93e2cf42d61831dd04
vzhang03 <vzhang@vassar.edu>
2024-06-17 12:15:29 -0400

    Continued work updating plugins

--- packages/plugin-fullscreen/src/index.ts
+++ packages/plugin-fullscreen/src/index.ts
@@ -1,36 +1,49 @@
 import { JsPsych, JsPsychPlugin, ParameterType, TrialType } from "jspsych";
 
+import { version } from "../package.json";
+
 const info = <const>{
   name: "fullscreen",
+  version: version,
   parameters: {
-    /** If true, experiment will enter fullscreen mode. If false, the browser will exit fullscreen mode. */
+    /** A value of `true` causes the experiment to enter fullscreen mode. A value of `false` causes the browser to exit fullscreen mode. */
     fullscreen_mode: {
       type: ParameterType.BOOL,
-      pretty_name: "Fullscreen mode",
       default: true,
       array: false,
     },
-    /** HTML content to display above the button to enter fullscreen mode */
+    /** `<p>The experiment will switch to full screen mode when you press the button below</p>` | The HTML content to display above the button to enter fullscreen mode. */
     message: {
       type: ParameterType.HTML_STRING,
-      pretty_name: "Message",
       default:
         "<p>The experiment will switch to full screen mode when you press the button below</p>",
       array: false,
     },
-    /** The text that appears on the button to enter fullscreen */
+    /** The text that appears on the button to enter fullscreen mode. */
     button_label: {
       type: ParameterType.STRING,
-      pretty_name: "Button label",
       default: "Continue",
       array: false,
     },
-    /** The length of time to delay after entering fullscreen mode before ending the trial. */
+    /** The length of time to delay after entering fullscreen mode before ending the trial. This can be useful because entering fullscreen is jarring and most browsers display some kind of message that the browser has entered fullscreen mode. */
     delay_after: {
       type: ParameterType.INT,
-      pretty_name: "Delay after",
       default: 1000,
       array: false,
+    },
+  },
+  data: {
+    /** true if the browser supports fullscreen mode (i.e., is not Safari) */
+    success: {
+      type: ParameterType.BOOL,
+      default: null,
+      description: "True if the user entered fullscreen mode, false if not.",
+    },
+    /** Response time to click the button that launches fullscreen mode */
+    rt: {
+      type: ParameterType.INT,
+      default: null,
+      description: "Time in milliseconds until the user entered fullscreen mode.",
     },
   },
 };
@@ -38,12 +51,13 @@ const info = <const>{
 type Info = typeof info;
 
 /**
- * **fullscreen**
+ * The fullscreen plugin allows the experiment to enter or exit fullscreen mode. For security reasons, all browsers require that entry into fullscreen mode is triggered by a user action. To enter fullscreen mode, this plugin has the user click a button. Exiting fullscreen mode can be done without user input.
  *
- * jsPsych plugin for toggling fullscreen mode in the browser
+ * !!! warning
+ *     Safari does not support keyboard input when the browser is in fullscreen mode. Therefore, the function will not launch fullscreen mode on Safari. The experiment will ignore any trials using the fullscreen plugin in Safari.
  *
  * @author Josh de Leeuw
- * @see {@link https://www.jspsych.org/plugins/jspsych-fullscreen/ fullscreen plugin documentation on jspsych.org}
+ * @see {@link https://www.jspsych.org/latest/plugins/fullscreen/ fullscreen plugin documentation on jspsych.org}
  */
 class FullscreenPlugin implements JsPsychPlugin<Info> {
   static info = info;
