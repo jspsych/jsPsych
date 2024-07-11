2d400d4c80e0703b70430703d90f786068bcce6e
vzhang03 <vzhang@vassar.edu>
2024-07-11 17:09:33 -0400

    Finished reading paths to directory names and paths to data

--- packages/metadata/cli/cli.js
+++ packages/metadata/cli/cli.js
@@ -4,28 +4,50 @@ import path from "path";
 import JsPsychMetadata from "../dist/index.js";
 
 const metadata = new JsPsychMetadata();
-const directoryPath = process.cwd() + "/test"; // can make this the path that is given (relative)
-console.log(directoryPath);
+// console.log("arg 0", process.argv[0]);
+// console.log("arg 1", process.argv[1]);
+// console.log("arg 2", process.argv[2], "typeof", typeof process.argv[2]);
 
-fs.readdir(directoryPath, (err, files) => {
-  if (err) {
+const relativePath = process.argv[2] || "../../../backend/mockdata/"; // process.argv[2] |
+const directoryPath = path.resolve(process.cwd(), relativePath);
+
+var metadata_options;
+
+if (process.argv[3]) {
+  const metadata_options_path = path.resolve(process.cwd(), process.argv[3]);
+
+  try {
+    const data = fs.readFileSync(metadata_options_path, "utf8"); // synchronous read
+    console.log("metadata options:", data); // log the raw data
+    metadata_options = JSON.parse(data); // parse the JSON data
+  } catch (error) {
+    console.error("Error reading or parsing metadata options:", error);
+  }
+}
+
+const processData = async () => {
+  try {
+    const files = await fs.promises.readdir(directoryPath);
+
+    for (const file of files) {
+      const filePath = path.join(directoryPath, file);
+
+      try {
+        const data = await fs.promises.readFile(filePath, "utf8");
+
+        console.log(`Reading file: ${file}:`);
+
+        await metadata.generate(data, metadata_options);
+      } catch (err) {
+        console.error(`Error reading file ${file}:`, err);
+      }
+    }
+  } catch (err) {
     console.error("Error reading directory:", err);
-    return;
   }
 
-  files.forEach((file) => {
-    const filePath = path.join(directoryPath, file);
+  await console.log(metadata.getMetadata());
+};
 
-    // Read each file asynchronously
-    fs.readFile(filePath, "utf8", (err, data) => {
-      if (err) {
-        console.error(`Error reading file ${file}:`, err);
-        return;
-      }
-      console.log(`Contents of ${file}:`);
-      console.log(data); // Output the contents of the file
-    });
-  });
-});
-
-// console.log("metadata:", metadata.getMetadata());
+// Call the processFiles function
+processData();
