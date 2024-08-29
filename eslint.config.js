7aebc69ba629f2ccef50ee4944102dfab607e2a3
joshunrau <contact@joshuaunrau.com>
2024-08-29 11:16:23 -0400

    allow unused vars starting with _ in eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -17,6 +17,18 @@ export default tslint.config(
   {
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          args: "all",
+          argsIgnorePattern: "^_",
+          caughtErrors: "all",
+          caughtErrorsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+        },
+      ],
     },
   }
 );
