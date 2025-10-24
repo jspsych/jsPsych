ceb8300293cffbaf53a3443d2613ff49f1220d01
Becky Gilbert <beckyannegilbert@gmail.com>
2025-10-24 15:58:02 -0700

    fix references to incorrect surveyjs version

--- packages/plugin-survey/src/index.ts
+++ packages/plugin-survey/src/index.ts
@@ -70,7 +70,7 @@ const info = <const>{
 type Info = typeof info;
 
 /**
- * SurveyJS version: 2.2.0
+ * SurveyJS version: 2.3.12
  *
  * This plugin is a wrapper for the [**SurveyJS form library**](https://surveyjs.io/form-library/documentation/overview). It displays survey-style questions across one or more pages. You can mix different question types on the same page, and participants can navigate back and forth through multiple survey pages without losing responses. SurveyJS provides a large number of built-in question types, response validation options, conditional display options, special response options ("None", "Select all", "Other"), and other useful features for building complex surveys. See the [Building Surveys in jsPsych](../overview/building-surveys.md) page for a more detailed list of all options and features.
  *
