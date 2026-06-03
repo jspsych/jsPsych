d71e28fbbd537c56495c5e547d17ee29db16506b
Ludan <ludanyuan@vassar.edu>
2026-06-03 10:39:49 -0400

    Remove strictRequires

--- packages/config/rollup.js
+++ packages/config/rollup.js
@@ -51,7 +51,6 @@ const makeConfig = ({
   /** @type{import("@rollup/plugin-commonjs").RollupCommonJSOptions} */
   const commonjsPluginOptions = {
     extensions: [".js", ".json"],
-    strictRequires: "auto",
   };
 
   // Non-babel builds
